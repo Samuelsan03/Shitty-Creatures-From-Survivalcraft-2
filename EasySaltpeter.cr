eb44e69c1be9b7d5877a6bcf7244fc7dc7dea803
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2025-11-21 13:30:25 -0500

    Add files via upload

---
+++ EasySaltpeter.cr
@@ -0,0 +1,28 @@
+<EasySaltpeter>
+<Recipes>
+    <Recipe Result="SaltpeterChunkBlock" ResultCount="40" RequiredHeatLevel="0" a="rottenpumpkinsoupbucket" Description="Refine saltpeter">
+      "a"      
+    </Recipe>
+    <Recipe Result="SaltpeterChunkBlock" ResultCount="40" RequiredHeatLevel="0" a="rottenmeat" Description="Easier than you think">
+    "a"
+    </Recipe>
+    <Recipe Result="SaltpeterChunkBlock" ResultCount="40" RequiredHeatLevel="0" a="rottenfish" Description="Easier than you think">
+    "a"
+    </Recipe>
+    <Recipe Result="SaltpeterChunkBlock" ResultCount="40" RequiredHeatLevel="0" a="rottenmilkbucket" Description="Easier than you think">
+    "a"
+    </Recipe>
+    <Recipe Result="SaltpeterChunkBlock" ResultCount="40" RequiredHeatLevel="0" a="rottenegg" Description="Easier than you think">
+    "a"
+    </Recipe>
+    <Recipe Result="SaltpeterChunkBlock" ResultCount="40" RequiredHeatLevel="0" a="rottenbird" Description="Easier than you think">
+    "a"
+    </Recipe>
+    <Recipe Result="SaltpeterChunkBlock" ResultCount="40" RequiredHeatLevel="0" a="rottendough" Description="Easier than you think">
+    "a"
+    </Recipe>
+    <Recipe Result="SaltpeterChunkBlock" ResultCount="40" RequiredHeatLevel="0" a="rottenpumpkin" Description="Easier than you think">
+    "a"
+    </Recipe>
+</Recipes>
+</EasySaltpeter>
