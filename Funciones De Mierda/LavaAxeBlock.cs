e0aaa4144c483b3fda89d94d7d9fa01a2287a32f
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2025-11-30 21:03:21 -0500

    Add files via upload

---
+++ Funciones De Mierda/LavaAxeBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Game
+{
+	// Token: 0x02000045 RID: 69
+	public class LavaAxeBlock : AxeBlock
+	{
+		// Token: 0x0600019F RID: 415 RVA: 0x0000AF4F File Offset: 0x0000914F
+		public LavaAxeBlock() : base(47, 126)
+		{
+		}
+
+		// Token: 0x040000C6 RID: 198
+		public static int Index = 911;
+	}
+}
---
+++ Funciones De Mierda/StonePickaxeBlock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Game
+{
+	// Token: 0x02000045 RID: 69
+	public class StonePickaxeBlock : PickaxeBlock
+	{
+		// Token: 0x0600019F RID: 415 RVA: 0x0000AF4F File Offset: 0x0000914F
+		public StonePickaxeBlock() : base(47, 1)
+		{
+		}
+
+		// Token: 0x040000C6 RID: 198
+		public static int Index = 1117;
+	}
+}
+
