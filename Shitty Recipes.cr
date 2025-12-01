e3ef6f01509259e83ab0508a15e982328384a78e
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2025-11-30 20:59:50 -0500

    Add files via upload

---
+++ Shitty Recipes.cr
@@ -0,0 +1,84 @@
+<ShittyRecipes>
+    <Recipes>
+<Recipe Result="WoodMacheteBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="woodenslab" Description="Just like the old days">
+      "b"
+      "b"
+      "a"
+    </Recipe>
+<Recipe Result="WoodAxeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="planks" Description="Just like the old days">
+      "bb"
+      "ba"
+      " a"
+    </Recipe>
+<Recipe Result="WoodPickaxeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="planks" Description="Just like the old days">
+      "bbb"
+      " a "
+      " a"
+    </Recipe>
+    <Recipe Result="WoodShovelBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="planks" Description="Just like the old days">
+      "b"
+      "a"
+      "a"
+    </Recipe>          
+    <Recipe Result="StonePickaxeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestone" Description="Just like the old days">
+      "bbb"
+      " a "
+      " a"
+    </Recipe>              
+    <Recipe Result="StoneAxeOriginalBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestoneslab" Description="Just like the old days">
+      "bb"
+      "ba"
+      " a"
+    </Recipe>          
+    <Recipe Result="StoneMacheteBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestoneslab" Description="Just like the old days">
+      "b"
+      "b"
+      "a"
+    </Recipe> 
+    <Recipe Result="StoneRakeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestone" Description="Just like the old days">
+      "b b"
+      " a "
+      " a "
+    </Recipe>       
+    <Recipe Result="StoneShovelBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestoneslab" Description="Just like the old days">
+      "b"
+      "a"
+      "a"
+    </Recipe>    
+    <Recipe Result="LavaSpearBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="magmabucket" Description="Just like the old days">
+      " b "
+      "bab"
+      " a "
+    </Recipe>       
+    <Recipe Result="LavaMacheteBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="magmabucket" Description="Just like the old days">
+      "b"
+      "b"
+      "a"
+    </Recipe>        
+    <Recipe Result="LavaAxeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="magmabucket" Description="Just like the old days">
+      "bb"
+      "ba"
+      " a"
+    </Recipe>  
+    <Recipe Result="LavaPickaxeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="magmabucket" Description="Just like the old days">
+      "bbb"
+      " a "
+      " a "
+    </Recipe>  
+    <Recipe Result="LavaHammerBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="magmabucket" Description="Just like the old days">
+      "bbb"
+      "bab"
+      " a "
+    </Recipe>
+    <Recipe Result="LavaRakeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="magmabucket" Description="Just like the old days">
+      "b b"
+      " a "
+      " a "
+    </Recipe>
+    <Recipe Result="LavaShovelBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="magmabucket" Description="Just like the old days">
+      "b"
+      "a"
+      "a"
+    </Recipe>
+    </Recipes>
+</ShittyRecipes>
