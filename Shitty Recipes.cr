602a57b9cab46291484bbe6d17a3cf32e3611a4e
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2025-12-05 20:20:59 -0500

    Update Shitty Recipes.cr

--- Shitty Recipes.cr
+++ Shitty Recipes.cr
@@ -1,84 +1,111 @@
 <ShittyRecipes>
     <Recipes>
-<Recipe Result="WoodMacheteBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="woodenslab" Description="Just like the old days">
-      "b"
-      "b"
-      "a"
-    </Recipe>
-<Recipe Result="WoodAxeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="planks" Description="Just like the old days">
-      "bb"
-      "ba"
-      " a"
-    </Recipe>
-<Recipe Result="WoodPickaxeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="planks" Description="Just like the old days">
-      "bbb"
-      " a "
-      " a"
-    </Recipe>
-    <Recipe Result="WoodShovelBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="planks" Description="Just like the old days">
-      "b"
-      "a"
-      "a"
-    </Recipe>          
-    <Recipe Result="StonePickaxeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestone" Description="Just like the old days">
-      "bbb"
-      " a "
-      " a"
-    </Recipe>              
-    <Recipe Result="StoneAxeOriginalBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestoneslab" Description="Just like the old days">
-      "bb"
-      "ba"
-      " a"
-    </Recipe>          
-    <Recipe Result="StoneMacheteBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestoneslab" Description="Just like the old days">
-      "b"
-      "b"
-      "a"
-    </Recipe> 
-    <Recipe Result="StoneRakeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestone" Description="Just like the old days">
-      "b b"
-      " a "
-      " a "
-    </Recipe>       
-    <Recipe Result="StoneShovelBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestoneslab" Description="Just like the old days">
-      "b"
-      "a"
-      "a"
-    </Recipe>    
-    <Recipe Result="LavaSpearBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="magmabucket" Description="Just like the old days">
-      " b "
-      "bab"
-      " a "
-    </Recipe>       
-    <Recipe Result="LavaMacheteBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="magmabucket" Description="Just like the old days">
-      "b"
-      "b"
-      "a"
-    </Recipe>        
-    <Recipe Result="LavaAxeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="magmabucket" Description="Just like the old days">
-      "bb"
-      "ba"
-      " a"
-    </Recipe>  
-    <Recipe Result="LavaPickaxeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="magmabucket" Description="Just like the old days">
-      "bbb"
-      " a "
-      " a "
-    </Recipe>  
-    <Recipe Result="LavaHammerBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="magmabucket" Description="Just like the old days">
-      "bbb"
-      "bab"
-      " a "
-    </Recipe>
-    <Recipe Result="LavaRakeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="magmabucket" Description="Just like the old days">
-      "b b"
-      " a "
-      " a "
-    </Recipe>
-    <Recipe Result="LavaShovelBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="magmabucket" Description="Just like the old days">
-      "b"
-      "a"
-      "a"
-    </Recipe>
+        <Recipe Result="WoodMacheteBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="woodenslab" Description="Classic tool from version 2.0">
+            "b"
+            "b"
+            "a"
+        </Recipe>
+        <Recipe Result="WoodAxeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="planks" Description="Back to basics - like the old days">
+            "bb"
+            "ba"
+            " a"
+        </Recipe>
+        <Recipe Result="WoodPickaxeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="planks" Description="Nostalgic mining - pre-2.1 era">
+            "bbb"
+            " a "
+            " a"
+        </Recipe>
+        <Recipe Result="WoodShovelBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="planks" Description="Simple digging - back when life was easier">
+            "b"
+            "a"
+            "a"
+        </Recipe>
+        <Recipe Result="StonePickaxeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestone" Description="How could I forget the old days, bro?">
+            "bbb"
+            " a "
+            " a"
+        </Recipe>
+        <Recipe Result="StoneAxeOriginalBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestoneslab" Description="Primitive but effective - pre-2.2">
+            "bb"
+            "ba"
+            " a"
+        </Recipe>
+        <Recipe Result="StoneMacheteBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestoneslab" Description="Back when stone could cut - version 2.0 memories">
+            "b"
+            "b"
+            "a"
+        </Recipe>
+        <Recipe Result="StonePickaxeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestoneslab" Description="How could I forget the old days, bro?">
+            "bbb"
+            " a "
+            " a "
+        </Recipe>
+        <Recipe Result="StoneRakeBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestone" Description="Ancient farming tool - back to roots">
+            "b b"
+            " a "
+            " a "
+        </Recipe>
+        <Recipe Result="StoneShovelBlock" ResultCount="1" RequiredHeatLevel="0" a="stick" b="cobblestoneslab" Description="Stone age technology revived">
+            "b"
+            "a"
+            "a"
+        </Recipe>
+        <!-- Lava Tools require level 6 -->
+        <Recipe Result="LavaSpearBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="6" a="stick" b="magmabucket" Description="Forged in magma - burns through anything">
+            " b "
+            "bab"
+            " a "
+        </Recipe>
+        <Recipe Result="LavaMacheteBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="6" a="stick" b="magmabucket" Description="Molten blade that dismembers with burning precision">
+            "b"
+            "b"
+            "a"
+        </Recipe>
+        <Recipe Result="LavaAxeBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="6" a="stick" b="magmabucket" Description="Chops wood with burning fury">
+            "bb"
+            "ba"
+            " a"
+        </Recipe>
+        <Recipe Result="LavaPickaxeBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="6" a="stick" b="magmabucket" Description="Mines ores with molten power">
+            "bbb"
+            " a "
+            " a "
+        </Recipe>
+        <Recipe Result="LavaHammerBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="6" a="stick" b="magmabucket" Description="Smashes rocks with volcanic force">
+            "bbb"
+            "bab"
+            " a "
+        </Recipe>
+        <Recipe Result="LavaRakeBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="6" a="stick" b="magmabucket" Description="Tills soil with volcanic efficiency">
+            "b b"
+            " a "
+            " a "
+        </Recipe>
+        <Recipe Result="LavaShovelBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="6" a="stick" b="magmabucket" Description="Digs through earth like it's butter - hot butter">
+            "b"
+            "a"
+            "a"
+        </Recipe>
+        <Recipe Result="RepeatCrossbowBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="6" a="rod" b="string" c="ironingot" d="planks" Description="Automatic crossbow with 8-arrow magazine - complex mechanical design">
+    " a "
+    "aba"
+    "dcd"
+</Recipe>
+<Recipe Result="RepeatArrowBlock:0" ResultCount="8" RequiredHeatLevel="0" RequiredPlayerLevel="6" a="rod" b="copperingot" c="feather" Description="Copper bolts - optimized for repeating mechanism">
+    " b "
+    "aca"
+</Recipe>
+<Recipe Result="RepeatArrowBlock:1" ResultCount="8" RequiredHeatLevel="0" RequiredPlayerLevel="6" a="rod" b="ironingot" c="feather" Description="Iron bolts - armor piercing for repeat fire">
+    " b "
+    "aca"
+</Recipe>
+<Recipe Result="RepeatArrowBlock:2" ResultCount="8" RequiredHeatLevel="0" RequiredPlayerLevel="6" a="rod" b="diamond" c="feather" Description="Diamond bolts - premium ammunition">
+    " b "
+    "aca"
+</Recipe>
+<Recipe Result="RepeatArrowBlock:3" ResultCount="6" RequiredHeatLevel="0" RequiredPlayerLevel="6" a="rod" b="gunpowder" c="feather" d="sulphurchunk" Description="Explosive bolts - devastating area damage">
+    "bd "
+    "aca"
+</Recipe>
     </Recipes>
 </ShittyRecipes>
