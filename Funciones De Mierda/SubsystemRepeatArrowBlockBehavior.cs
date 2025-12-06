ebec007ac8956b64ecc3e8a7a20caa5a488d3ef6
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2025-12-05 19:10:01 -0500

    Create SubsystemRepeatArrowBlockBehavior.cs

---
+++ Funciones De Mierda/SubsystemRepeatArrowBlockBehavior.cs
@@ -0,0 +1,64 @@
+using System;
+using Engine;
+using Game;
+using TemplatesDatabase;
+
+namespace Game
+{
+	public class SubsystemRepeatArrowBlockBehavior : SubsystemBlockBehavior
+	{
+		public override int[] HandledBlocks
+		{
+			get
+			{
+				return Array.Empty<int>();
+			}
+		}
+
+		public override void OnFiredAsProjectile(Projectile projectile)
+		{
+			if (RepeatArrowBlock.GetArrowType(Terrain.ExtractData(projectile.Value)) != RepeatArrowBlock.ArrowType.ExplosiveArrow)
+			{
+				return;
+			}
+			this.m_subsystemProjectiles.AddTrail(projectile, Vector3.Zero, new SmokeTrailParticleSystem(20, 0.5f, float.MaxValue, Color.White));
+			projectile.ProjectileStoppedAction = ProjectileStoppedAction.Disappear;
+		}
+
+		public override bool OnHitAsProjectile(CellFace? cellFace, ComponentBody componentBody, WorldItem worldItem)
+		{
+			RepeatArrowBlock.ArrowType arrowType = RepeatArrowBlock.GetArrowType(Terrain.ExtractData(worldItem.Value));
+
+			// Removed all poison-related code
+
+			if (worldItem.Velocity.Length() > 10f)
+			{
+				float breakChance = 0f;
+
+				switch (arrowType)
+				{
+					case RepeatArrowBlock.ArrowType.CopperArrow:
+						breakChance = 0.15f;
+						break;
+				}
+
+				if (this.m_random.Float(0f, 1f) < breakChance)
+				{
+					// Si se rompe la flecha, desaparece (no se convierte en otro tipo)
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public override void Load(ValuesDictionary valuesDictionary)
+		{
+			base.Load(valuesDictionary);
+			this.m_subsystemProjectiles = base.Project.FindSubsystem<SubsystemProjectiles>(true);
+		}
+
+		public SubsystemProjectiles m_subsystemProjectiles;
+
+		public Engine.Random m_random = new Engine.Random();
+	}
+}
