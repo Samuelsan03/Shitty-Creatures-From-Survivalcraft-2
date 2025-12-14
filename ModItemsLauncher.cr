60d8c030b821aa34cd0bc83aee9496bc4593860f
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2025-12-14 17:25:58 -0500

    Delete ModItemsLauncher.cr

--- ModItemsLauncher.cr
+++
@@ -1,8 +0,0 @@
-﻿<?xml version="1.0" encoding="utf-8"?>
-<ModItemsLauncher>
-	<Weapons>
-		<Recipe Result="ItemsLauncherBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="10" a="copperblock" b="musket" c="rod" Description="Make an object launcher by modifying a new musket.">
-			"bca"
-		</Recipe>
-	</Weapons>
-</ModItemsLauncher>
