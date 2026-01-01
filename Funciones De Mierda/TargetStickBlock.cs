dafb42d3b9edd3b9d85cf0bcc362f008ceb5343b
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-01-01 15:31:23 -0500

    Create TargetStickBlock.cs

---
+++ Funciones De Mierda/TargetStickBlock.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Game
+{
+	// Token: 0x0200001B RID: 27
+	public class TargetStickBlock : Armas
+	{
+		// Token: 0x060000AD RID: 173 RVA: 0x00008BE8 File Offset: 0x00006DE8
+		public TargetStickBlock() : base("Models/Vara", "Textures/Items/Vara")
+		{
+		}
+
+		// Token: 0x040000C5 RID: 197
+		public const int Index = 329;
+	}
+}
