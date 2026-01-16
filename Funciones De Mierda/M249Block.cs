3e18784f26853b6382951d90bb3f5c85579db8bc
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-01-16 13:34:19 -0500

    Add files via upload

---
+++ Funciones De Mierda/M249Block.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Engine;
+using Engine.Graphics;
+
+namespace Game
+{
+	public class M249Block : Block
+	{
+		public override void Initialize()
+		{
+			Model model = ContentManager.Get<Model>("Models/Armas De Porqueria/M249", null);
+			Matrix boneAbsoluteTransform = BlockMesh.GetBoneAbsoluteTransform(model.FindMesh("Gun", true).ParentBone);
+			this.m_standaloneBlockMesh.AppendModelMeshPart(model.FindMesh("Gun", true).MeshParts[0], boneAbsoluteTransform * Matrix.CreateTranslation(0f, -0.1f, 0f), false, false, true, false, Color.White);
+			base.Initialize();
+		}
+
+		public override void GenerateTerrainVertices(BlockGeometryGenerator generator, TerrainGeometry geometry, int value, int x, int y, int z)
+		{
+			generator.GenerateShadedMeshVertices(this, x, y, z, this.m_standaloneBlockMesh, Color.White, null, null, geometry.SubsetOpaque);
+		}
+
+		public override void DrawBlock(PrimitivesRenderer3D primitivesRenderer, int value, Color color, float size, ref Matrix matrix, DrawBlockEnvironmentData environmentData)
+		{
+			BlocksManager.DrawMeshBlock(primitivesRenderer, this.m_standaloneBlockMesh, this.texture, color, 2.7f * size, ref matrix, environmentData);
+		}
+
+		public static int GetBulletNum(int data)
+		{
+			return (data >> 4) & 127; // 7 bits para 0-127 (capacidad del cargador real de la M249 es 100-200)
+		}
+
+		public static int SetBulletNum(int data, int bulletNum)
+		{
+			bulletNum = MathUtils.Clamp(bulletNum, 0, 100); // Capacidad real de la M249
+			return (data & ~2032) | ((bulletNum & 127) << 4); // 2032 = 127 << 4
+		}
+
+		public const int Index = 379; // Nuevo índice
+
+		private BlockMesh m_standaloneBlockMesh = new BlockMesh();
+
+		public Texture2D texture = ContentManager.Get<Texture2D>("Textures/Armas De Mierda/M249", null);
+	}
+}
---
+++ Funciones De Mierda/SubsystemNewG3Behavior.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Engine;
+using TemplatesDatabase;
+
+namespace Game
+{
+	public class SubsystemNewG3Behavior : SubsystemBlockBehavior
+	{
+		public override int[] HandledBlocks
+		{
+			get
+			{
+				return new int[]
+				{
+					BlocksManager.GetBlockIndex(typeof(NewG3Block), true, false)
+				};
+			}
+		}
+
+		public override void Load(ValuesDictionary valuesDictionary)
+		{
+			base.Load(valuesDictionary);
+			this.m_subsystemTerrain = base.Project.FindSubsystem<SubsystemTerrain>(true);
+			this.m_subsystemTime = base.Project.FindSubsystem<SubsystemTime>(true);
+			this.m_subsystemProjectiles = base.Project.FindSubsystem<SubsystemProjectiles>(true);
+			this.m_subsystemParticles = base.Project.FindSubsystem<SubsystemParticles>(true);
+			this.m_subsystemAudio = base.Project.FindSubsystem<SubsystemAudio>(true);
+			this.m_subsystemNoise = base.Project.FindSubsystem<SubsystemNoise>(true);
+			this.m_subsystemGameInfo = base.Project.FindSubsystem<SubsystemGameInfo>(true);
+			this.fire = true;
+		}
+
+		public override bool OnAim(Ray3 aim, ComponentMiner componentMiner, AimState state)
+		{
+			Vector3 vector = aim.Direction;
+			IInventory inventory = componentMiner.Inventory;
+			bool flag = inventory != null;
+			if (flag)
+			{
+				int activeSlotIndex = inventory.ActiveSlotIndex;
+				bool flag2 = activeSlotIndex >= 0;
+				if (flag2)
+				{
+					int slotValue = inventory.GetSlotValue(activeSlotIndex);
+					int slotCount = inventory.GetSlotCount(activeSlotIndex);
+					int num = Terrain.ExtractContents(slotValue);
+					int num2 = slotValue;
+					bool flag3 = num == BlocksManager.GetBlockIndex(typeof(NewG3Block), true, false) && slotCount > 0;
+					if (flag3)
+					{
+						double gameTime;
+						bool flag4 = !this.m_aimStartTimes.TryGetValue(componentMiner, out gameTime);
+						if (flag4)
+						{
+							gameTime = this.m_subsystemTime.GameTime;
+							this.m_aimStartTimes[componentMiner] = gameTime;
+						}
+						float num3 = (float)(this.m_subsystemTime.GameTime - gameTime);
+						float num4 = (float)MathUtils.Remainder(this.m_subsystemTime.GameTime, 1000.0);
+						Vector3 v = (float)((componentMiner.ComponentCreature.ComponentBody.IsSneaking ? 0.00999999977648258 : 0.0299999993294477) + 0.200000002980232 * (double)MathUtils.Saturate((num3 - 6.5f) / 40f)) * new Vector3
+						{
+							X = SimplexNoise.OctavedNoise(num4, 2f, 3, 2f, 0.5f, false),
+							Y = SimplexNoise.OctavedNoise(num4 + 100f, 2f, 3, 2f, 0.5f, false),
+							Z = SimplexNoise.OctavedNoise(num4 + 200f, 2f, 3, 2f, 0.5f, false)
+						};
+						bool flag5 = num3 > 1f;
+						if (flag5)
+						{
+							bool flag6 = 0.2f * num3 < 1.2f;
+							if (flag6)
+							{
+								vector.Y += 0.1f * (num3 - 1f);
+							}
+							else
+							{
+								vector.Y += 0.3f;
+							}
+						}
+						vector = Vector3.Normalize(vector + v * 2f);
+						switch (state)
+						{
+							case AimState.InProgress:
+								{
+									ComponentFirstPersonModel componentFirstPersonModel = componentMiner.Entity.FindComponent<ComponentFirstPersonModel>();
+									bool flag7 = componentFirstPersonModel != null;
+									if (flag7)
+									{
+										ComponentPlayer componentPlayer = componentMiner.ComponentPlayer;
+										bool flag8 = componentPlayer != null;
+										if (flag8)
+										{
+											componentPlayer.ComponentAimingSights.ShowAimingSights(aim.Position, vector);
+										}
+										componentFirstPersonModel.ItemOffsetOrder = new Vector3(-0.21f, 0.15f, 0.08f);
+										componentFirstPersonModel.ItemRotationOrder = new Vector3(-0.7f, 0f, 0f);
+									}
+									componentMiner.ComponentCreature.ComponentCreatureModel.AimHandAngleOrder = 1.4f;
+									componentMiner.ComponentCreature.ComponentCreatureModel.InHandItemOffsetOrder = new Vector3(-0.08f, -0.08f, 0.07f);
+									componentMiner.ComponentCreature.ComponentCreatureModel.InHandItemRotationOrder = new Vector3(-1.7f, 0f, 0f);
+									int bulletNum = NewG3Block.GetBulletNum(Terrain.ExtractData(slotValue));
+									bool flag9 = bulletNum > 0;
+									bool flag10 = this.m_subsystemTime.PeriodicGameTimeEvent(0.12, 0.0) && flag9;
+									if (flag10)
+									{
+										Vector3 vector2 = componentMiner.ComponentCreature.ComponentCreatureModel.EyePosition + componentMiner.ComponentCreature.ComponentBody.Matrix.Right * 0.3f - componentMiner.ComponentCreature.ComponentBody.Matrix.Up * 0.2f;
+										Vector3 vector3 = Vector3.Normalize(vector2 + vector * 10f - vector2);
+										Vector3 vector4 = Vector3.Normalize(Vector3.Cross(vector3, Vector3.UnitY));
+										Vector3 v2 = Vector3.Normalize(Vector3.Cross(vector3, vector4));
+										int num5 = 2;
+										Vector3 vector5 = new Vector3(0.009f, 0.009f, 0.04f);
+										for (int i = 0; i < num5; i++)
+										{
+											int value = Terrain.MakeBlockValue(BlocksManager.GetBlockIndex(typeof(NuevaBala3), true, false), 0, 2);
+											Vector3 v3 = this.m_random.Float(-vector5.X, vector5.X) * vector4 + this.m_random.Float(-vector5.Y, vector5.Y) * v2 + this.m_random.Float(-vector5.Z, vector5.Z) * vector3;
+											this.m_subsystemProjectiles.FireProjectile(value, vector2, 290f * (vector3 + v3), Vector3.Zero, componentMiner.ComponentCreature);
+										}
+										this.m_subsystemAudio.PlaySound("Audio/Armas/G3 fuego", 1f, this.m_random.Float(-0.1f, 0.1f), componentMiner.ComponentCreature.ComponentCreatureModel.EyePosition, 10f, true);
+										Vector3 vector6 = Vector3.Normalize(componentMiner.ComponentPlayer.GameWidget.ActiveCamera.ViewDirection);
+										this.m_subsystemParticles.AddParticleSystem(new GunFireParticleSystem(this.m_subsystemTerrain, vector2 + 1.3f * vector6, vector6), false);
+										this.m_subsystemNoise.MakeNoise(vector2, 1f, 40f);
+										int bulletNum2 = bulletNum - 1;
+										num2 = Terrain.MakeBlockValue(BlocksManager.GetBlockIndex(typeof(NewG3Block), true, false), 0, NewG3Block.SetBulletNum(bulletNum2));
+										ComponentPlayer componentPlayer2 = componentMiner.ComponentPlayer;
+										bool flag11 = componentPlayer2 != null;
+										if (flag11)
+										{
+											componentPlayer2.ComponentGui.DisplaySmallMessage(bulletNum2.ToString(), Color.White, true, false);
+										}
+										bool flag12 = componentFirstPersonModel != null;
+										if (flag12)
+										{
+											componentFirstPersonModel.ItemRotationOrder = new Vector3(-0.85f, 0f, 0f);
+										}
+									}
+									else
+									{
+										bool flag13 = !flag9 && this.m_subsystemTime.PeriodicGameTimeEvent(0.5, 0.0);
+										if (flag13)
+										{
+											this.m_subsystemAudio.PlaySound("Audio/WeaponDryFire", 0.7f, this.m_random.Float(-0.1f, 0.1f), componentMiner.ComponentCreature.ComponentCreatureModel.EyePosition, 3f, true);
+										}
+									}
+									break;
+								}
+							case AimState.Cancelled:
+								{
+									this.m_aimStartTimes.Remove(componentMiner);
+									bool flag14 = num2 != slotValue;
+									if (flag14)
+									{
+										inventory.RemoveSlotItems(activeSlotIndex, 1);
+										inventory.AddSlotItems(activeSlotIndex, num2, 1);
+									}
+									componentMiner.ComponentCreature.ComponentCreatureModel.InHandItemOffsetOrder = Vector3.Zero;
+									componentMiner.ComponentCreature.ComponentCreatureModel.InHandItemRotationOrder = Vector3.Zero;
+									componentMiner.ComponentCreature.ComponentCreatureModel.AimHandAngleOrder = 0f;
+									ComponentFirstPersonModel componentFirstPersonModel2 = componentMiner.Entity.FindComponent<ComponentFirstPersonModel>();
+									bool flag15 = componentFirstPersonModel2 != null;
+									if (flag15)
+									{
+										componentFirstPersonModel2.ItemOffsetOrder = Vector3.Zero;
+										componentFirstPersonModel2.ItemRotationOrder = Vector3.Zero;
+									}
+									break;
+								}
+							case AimState.Completed:
+								{
+									this.m_aimStartTimes.Remove(componentMiner);
+									bool flag16 = num2 != slotValue;
+									if (flag16)
+									{
+										inventory.RemoveSlotItems(activeSlotIndex, 1);
+										inventory.AddSlotItems(activeSlotIndex, num2, 1);
+									}
+									componentMiner.ComponentCreature.ComponentCreatureModel.InHandItemOffsetOrder = Vector3.Zero;
+									componentMiner.ComponentCreature.ComponentCreatureModel.InHandItemRotationOrder = Vector3.Zero;
+									componentMiner.ComponentCreature.ComponentCreatureModel.AimHandAngleOrder = 0f;
+									ComponentFirstPersonModel componentFirstPersonModel3 = componentMiner.Entity.FindComponent<ComponentFirstPersonModel>();
+									bool flag17 = componentFirstPersonModel3 != null;
+									if (flag17)
+									{
+										componentFirstPersonModel3.ItemOffsetOrder = Vector3.Zero;
+										componentFirstPersonModel3.ItemRotationOrder = Vector3.Zero;
+									}
+									break;
+								}
+						}
+						bool flag18 = state == AimState.InProgress && num2 != slotValue;
+						if (flag18)
+						{
+							inventory.RemoveSlotItems(activeSlotIndex, 1);
+							inventory.AddSlotItems(activeSlotIndex, num2, 1);
+						}
+					}
+				}
+			}
+			return false;
+		}
+
+		public override int GetProcessInventoryItemCapacity(IInventory inventory, int slotIndex, int value)
+		{
+			bool flag = value == BlocksManager.GetBlockIndex(typeof(NewG3Bullet), true, false) && NewG3Block.GetBulletNum(Terrain.ExtractData(inventory.GetSlotValue(slotIndex))) < 30;
+			bool flag2 = flag;
+			int result;
+			if (flag2)
+			{
+				result = 1;
+			}
+			else
+			{
+				result = 0;
+			}
+			return result;
+		}
+
+		public override void ProcessInventoryItem(IInventory inventory, int slotIndex, int value, int count, int processCount, out int processedValue, out int processedCount)
+		{
+			processedValue = value;
+			processedCount = count;
+			int bulletNum = NewG3Block.GetBulletNum(Terrain.ExtractData(inventory.GetSlotValue(slotIndex)));
+			bool flag = bulletNum < 30;
+			bool flag2 = flag;
+			if (flag2)
+			{
+				processedValue = 0;
+				processedCount = 0;
+				inventory.RemoveSlotItems(slotIndex, 1);
+				inventory.AddSlotItems(slotIndex, Terrain.MakeBlockValue(BlocksManager.GetBlockIndex(typeof(NewG3Block), true, false), 0, NewG3Block.SetBulletNum(30)), 1);
+			}
+		}
+
+		public SubsystemTerrain m_subsystemTerrain;
+		public SubsystemTime m_subsystemTime;
+		public SubsystemProjectiles m_subsystemProjectiles;
+		public SubsystemParticles m_subsystemParticles;
+		public SubsystemAudio m_subsystemAudio;
+		private SubsystemGameInfo m_subsystemGameInfo;
+		public SubsystemNoise m_subsystemNoise;
+		public Game.Random m_random = new Game.Random();
+		public Dictionary<ComponentMiner, double> m_aimStartTimes = new Dictionary<ComponentMiner, double>();
+		public bool fire;
+	}
+}
