20129286fbcfdf1d82a67c3469b3e09ff88deef1
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-01-26 15:04:55 -0500

    Add files via upload

---
+++ Funciones De Mierda/ComponentFlushInventory.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Game;
+using GameEntitySystem;
+using TemplatesDatabase;
+
+namespace WonderfulEra
+{
+	public class ComponentFlushInventory : ComponentInventoryBase, IUpdateable
+	{
+		public override void Load(ValuesDictionary valuesDictionary, IdToEntityMap idToEntityMap)
+		{
+			base.Load(valuesDictionary, idToEntityMap);
+			this.m_subsystemTime = base.Project.FindSubsystem<SubsystemTime>();
+			foreach (Block item in from blockStr in valuesDictionary.GetValue<string>("ValidBlocks").Split(',', StringSplitOptions.None).ToList<string>()
+								   select BlocksManager.GetBlock(blockStr, false) into block
+								   where block != null
+								   select block)
+			{
+				this.m_validBlocks.Add(item);
+			}
+			this.m_updateInterval = Math.Max(10.0, double.Parse(valuesDictionary.GetValue<string>("UpdateInterval", "180")));
+			this.m_currentTime = valuesDictionary.GetValue<double>("CurrentTime", 0.0);
+			this.m_nextCheckTime = valuesDictionary.GetValue<double>("NextCheckTime", 0.0);
+		}
+
+		public UpdateOrder UpdateOrder
+		{
+			get
+			{
+				return (UpdateOrder)202;
+			}
+		}
+
+		public void Update(float dt)
+		{
+			this.m_currentTime += (double)this.m_subsystemTime.m_gameTimeDelta;
+			if (this.m_currentTime < this.m_nextCheckTime)
+			{
+				return;
+			}
+			if (this.SlotsCount <= 0 || this.m_validBlocks.Count <= 0)
+			{
+				return;
+			}
+			if (base.Entity.ValuesDictionary.DatabaseObject.Name.Equals("Elk_Car"))
+			{
+				this.AddBitchItems();
+				this.m_nextCheckTime = this.m_currentTime + this.m_updateInterval;
+				return;
+			}
+			int num = this.m_random.Int(0, this.SlotsCount);
+			if (this.m_slots[num] != null && this.m_slots[num].Count > 0)
+			{
+				this.m_nextCheckTime = this.m_currentTime + this.m_updateInterval;
+				return;
+			}
+			int index = this.m_random.Int(0, this.m_validBlocks.Count);
+			Block block = this.m_validBlocks[index];
+			int count = 1;
+			this.AddSlotItems(num, Terrain.MakeBlockValue(block.BlockIndex, 0, 0), count);
+			this.m_nextCheckTime = this.m_currentTime + this.m_updateInterval;
+		}
+
+		private void AddBitchItems()
+		{
+			List<Block> list = this.SelectRandomEleWithPartialShuffle(this.m_validBlocks, this.m_random.Int(this.SlotsCount / 3, this.SlotsCount));
+			int index = 0;
+			foreach (Block block in list)
+			{
+				if (this.m_slots[index] != null && this.m_slots[index].Count > 0)
+				{
+					this.m_nextCheckTime = this.m_currentTime + this.m_updateInterval;
+					break;
+				}
+				if (!this.m_random.Bool(0.9f))
+				{
+					int count = 1;
+					this.AddSlotItems(index++, Terrain.MakeBlockValue(block.BlockIndex, 0, 0), count);
+				}
+			}
+		}
+
+		public List<Block> SelectRandomEleWithPartialShuffle(List<Block> list, int count)
+		{
+			List<Block> list2 = new List<Block>(list);
+			int num = Math.Min(count, list2.Count);
+			for (int i = 0; i < num; i++)
+			{
+				int num2 = this.m_random.Int(i, list2.Count);
+				List<Block> list3 = list2;
+				int index = i;
+				List<Block> list4 = list2;
+				int index2 = num2;
+				Block value = list2[num2];
+				Block value2 = list2[i];
+				list3[index] = value;
+				list4[index2] = value2;
+			}
+			return list2.Take(num).ToList<Block>();
+		}
+
+		public override void Save(ValuesDictionary valuesDictionary, EntityToIdMap entityToIdMap)
+		{
+			valuesDictionary.SetValue<double>("CurrentTime", this.m_currentTime);
+			valuesDictionary.SetValue<double>("NextCheckTime", this.m_nextCheckTime);
+			base.Save(valuesDictionary, entityToIdMap);
+		}
+
+		public List<Block> m_validBlocks = new List<Block>();
+
+		private SubsystemTime m_subsystemTime;
+
+		private double m_updateInterval;
+
+		private double m_currentTime;
+
+		private double m_nextCheckTime;
+	}
+}
---
+++ Funciones De Mierda/PirateNpcWidget.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Globalization;
+using System.Xml.Linq;
+using Engine;
+using Game;
+
+namespace WonderfulEra
+{
+	// Token: 0x0200011C RID: 284
+	public class PirateNpcWidget : CanvasWidget
+	{
+		// Token: 0x0600087B RID: 2171 RVA: 0x0003F724 File Offset: 0x0003D924
+		public PirateNpcWidget(IInventory inventory, ComponentTradeFlushInventory tradeFlushInventory)
+		{
+			this.m_tradeFlushInventory = tradeFlushInventory;
+			this.m_inventory = inventory;
+			XElement node = ContentManager.Get<XElement>("Widgets/PirateNpcWidget");
+			this.LoadContents(this, node);
+			this.m_inventoryGrid = this.Children.Find<GridPanelWidget>("InventoryGrid", true);
+			this.m_chestGrid = this.Children.Find<GridPanelWidget>("PirateNpcGrid", true);
+			this.m_tradeButton = this.Children.Find<ButtonWidget>("TradeButton", true);
+			this.m_tradeButton.Color = Color.Green;
+			InventorySlotWidget inventorySlotWidget = this.Children.Find<InventorySlotWidget>("TradeSellSlot", true);
+			InventorySlotWidget inventorySlotWidget2 = this.Children.Find<InventorySlotWidget>("TradeResultSlot", true);
+			inventorySlotWidget.CenterColor = Color.LightYellow;
+			inventorySlotWidget.BevelColor = Color.Yellow;
+			inventorySlotWidget2.CenterColor = Color.LightGreen;
+			inventorySlotWidget2.BevelColor = Color.Green;
+			int num = 0;
+			for (int i = 0; i < this.m_chestGrid.RowsCount; i++)
+			{
+				for (int j = 0; j < this.m_chestGrid.ColumnsCount; j++)
+				{
+					InventorySlotWidget inventorySlotWidget3 = new InventorySlotWidget();
+					inventorySlotWidget3.AssignInventorySlot(tradeFlushInventory, num++);
+					this.m_chestGrid.Children.Add(inventorySlotWidget3);
+					this.m_chestGrid.SetWidgetCell(inventorySlotWidget3, new Point2(j, i));
+				}
+			}
+			num = 10;
+			for (int k = 0; k < this.m_inventoryGrid.RowsCount; k++)
+			{
+				for (int l = 0; l < this.m_inventoryGrid.ColumnsCount; l++)
+				{
+					InventorySlotWidget inventorySlotWidget4 = new InventorySlotWidget();
+					inventorySlotWidget4.AssignInventorySlot(inventory, num++);
+					this.m_inventoryGrid.Children.Add(inventorySlotWidget4);
+					this.m_inventoryGrid.SetWidgetCell(inventorySlotWidget4, new Point2(l, k));
+				}
+			}
+			inventorySlotWidget.AssignInventorySlot(tradeFlushInventory, tradeFlushInventory.TradeSellSlotIndex);
+			inventorySlotWidget2.AssignInventorySlot(tradeFlushInventory, tradeFlushInventory.TradeResultSlotIndex);
+		}
+
+		// Token: 0x0600087C RID: 2172 RVA: 0x0003F8FC File Offset: 0x0003DAFC
+		public override void Update()
+		{
+			if (!this.m_tradeFlushInventory.IsAddedToProject)
+			{
+				base.ParentWidget.Children.Remove(this);
+				return;
+			}
+			this.Children.Find<LabelWidget>("Ratio", true).Text = this.m_tradeFlushInventory.Ratio.ToString(CultureInfo.CurrentCulture);
+			Color color = Color.White;
+			if ((double)this.m_tradeFlushInventory.Ratio > 1.0)
+			{
+				color = Color.Lerp(color, Color.DarkRed, this.m_tradeFlushInventory.Ratio - 1f);
+			}
+			else if ((double)this.m_tradeFlushInventory.Ratio < 1.0)
+			{
+				color = Color.Lerp(color, Color.DarkGreen, 1f - this.m_tradeFlushInventory.Ratio);
+			}
+			color.A = 192;
+			this.Children.Find<LabelWidget>("Ratio", true).Color = color;
+			int slotCount = this.m_tradeFlushInventory.GetSlotCount(this.m_tradeFlushInventory.ActiveSlotIndex);
+			int slotValue = this.m_tradeFlushInventory.GetSlotValue(this.m_tradeFlushInventory.ActiveSlotIndex);
+			if (slotValue != 0 && slotCount > 0)
+			{
+				ComponentTradeFlushInventory.Price price;
+				this.m_tradeButton.IsEnabled = this.m_tradeFlushInventory.CanTrade(this.m_inventory, out price);
+				if (this.m_tradeButton.IsClicked && this.m_tradeFlushInventory.TradeProcess(slotValue, slotCount, price))
+				{
+					AudioManager.PlaySound("Audio/CashRegister", 1f, 0f, 0f);
+				}
+				this.Children.Find<BlockIconWidget>("Currency", true).Value = price.CurrencyValue;
+				this.Children.Find<LabelWidget>("SellPrice", true).Text = MathUtils.Round((float)price.SellPrice * this.m_tradeFlushInventory.Ratio * (float)slotCount).ToString(CultureInfo.CurrentCulture);
+				return;
+			}
+			this.m_tradeButton.IsEnabled = false;
+		}
+
+		// Token: 0x04000575 RID: 1397
+		public IInventory m_inventory;
+
+		// Token: 0x04000576 RID: 1398
+		public ComponentTradeFlushInventory m_tradeFlushInventory;
+
+		// Token: 0x04000577 RID: 1399
+		public GridPanelWidget m_inventoryGrid;
+
+		// Token: 0x04000578 RID: 1400
+		public GridPanelWidget m_chestGrid;
+
+		// Token: 0x04000579 RID: 1401
+		private readonly ButtonWidget m_tradeButton;
+	}
+}
