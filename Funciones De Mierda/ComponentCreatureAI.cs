536c080d7e5a41c9c558265d5731199850a94d11
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-02-17 09:32:14 -0500

    Update ComponentCreatureAI.cs

--- Funciones De Mierda/ComponentCreatureAI.cs
+++ Funciones De Mierda/ComponentCreatureAI.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Engine;
 using GameEntitySystem;
@@ -107,8 +107,8 @@ namespace Game
 		private bool m_hasFirearmAimed = false;
 
 		private float m_maxDistance = 100f;
-		private float m_drawTime = 1.2f;
-		private float m_aimTime = 0.5f;
+		private float m_drawTime = 0.3f;
+		private float m_aimTime = 0.3f;
 		private float m_reloadTime = 0.8f;
 		private float m_cockTime = 0.5f;
 		private float m_flameShotInterval = 0.3f;
@@ -118,12 +118,12 @@ namespace Game
 		private float m_firearmAimTime = 0.5f;
 		private float m_firearmReloadTime = 1.0f;
 		private float m_sniperAimTime = 1.0f;
-		private float m_throwableAimTime = 0.5f;
+		private float m_throwableAimTime = 0.3f;
 
 		private float m_explosiveBoltMinDistance = 15f;
 		private float m_explosiveRepeatArrowMinDistance = 15f;
 
-		private float m_repeatCrossbowDrawTime = 0.5f;
+		private float m_repeatCrossbowDrawTime = 0.3f;
 		private float m_repeatCrossbowTimeBetweenShots = 0.5f;
 		private float m_repeatCrossbowMaxInaccuracy = 0.04f;
 		private float m_repeatCrossbowBoltSpeed = 35f;
