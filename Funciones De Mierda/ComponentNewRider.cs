3c19187b1c96b51a43dcfde6036cc08d8abaa131
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-02-28 19:31:40 -0500

    Create ComponentNewRider.cs

---
+++ Funciones De Mierda/ComponentNewRider.cs
@@ -0,0 +1,13 @@
+using System;
+using Engine;
+using GameEntitySystem;
+using TemplatesDatabase;
+
+namespace Game
+{
+	public class ComponentNewRider : ComponentRider
+	{
+		// Si quieres funcionalidades extra, las añades aquí
+		// Por ahora vacío, la herencia basta
+	}
+}
