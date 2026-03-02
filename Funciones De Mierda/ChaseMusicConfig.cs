d9b597f42ad77bd53f6b0c1e3784b40ad73a06c0
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-03-01 22:30:59 -0500

    Create ChaseMusicConfig.cs

---
+++ Funciones De Mierda/ChaseMusicConfig.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Game
+{
+	public static class ChaseMusicConfig
+	{
+		public static bool GhostMusicEnabled { get; set; } = true;
+		public static bool TankMusicEnabled { get; set; } = true;
+	}
+}
