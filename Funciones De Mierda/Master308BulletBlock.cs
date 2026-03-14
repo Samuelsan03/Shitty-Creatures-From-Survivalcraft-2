e20d3fe089413b0d90f46b06fe56ad6fae652c6c
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-03-14 11:42:11 -0500

    Create Master308BulletBlock.cs

---
+++ Funciones De Mierda/Master308BulletBlock.cs
@@ -0,0 +1,15 @@
+using Engine;
+using Engine.Graphics;
+
+namespace Game
+{
+	public class Master308BulletBlock : ShittyTexturesFlat
+	{
+		public const int Index = 407;
+
+		public Master308BulletBlock() : base("Textures/Armas De Mierda/38CL")
+		{
+			this.BlockIndex = Index;
+		}
+	}
+}
