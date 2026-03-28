0c0aea3a81871b95e22aa5839e411a60ffceda0b
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-03-28 11:30:57 -0500

    Update ShittyTexturesFlat.cs

--- Funciones De Mierda/ShittyTexturesFlat.cs
+++ Funciones De Mierda/ShittyTexturesFlat.cs
@@ -6,19 +6,14 @@ namespace Game
 {
 	public abstract class ShittyTexturesFlat : Block
 	{
-		// Token: 0x0400000C RID: 12
 		public Texture2D m_texture;
-
-		// Token: 0x0400000D RID: 13
 		public string texture;
 
-		// Token: 0x0600001B RID: 27 RVA: 0x0000233D File Offset: 0x0000053D
 		public ShittyTexturesFlat(string textureName)
 		{
 			this.texture = textureName;
-
 		}
-		// Token: 0x0600001C RID: 28 RVA: 0x0000244C File Offset: 0x0000064C
+
 		public override void Initialize()
 		{
 			base.Initialize();
@@ -28,13 +23,91 @@ namespace Game
 			}
 		}
 
-		// Token: 0x0600001D RID: 29 RVA: 0x00002466 File Offset: 0x00000666
+		// ¡IMPORTANTE! Generar geometría para el bloque colocado en el mundo
 		public override void GenerateTerrainVertices(BlockGeometryGenerator generator, TerrainGeometry geometry, int value, int x, int y, int z)
 		{
-			// Los bloques planos 2D no generan geometría de terreno
+			if (this.m_texture != null)
+			{
+				// Obtener la geometría para la textura personalizada
+				TerrainGeometry customGeometry = geometry.GetGeometry(this.m_texture);
+
+				// Generar un bloque plano en forma de cruz (como las plantas)
+				// Esto hace que la textura se vea desde todos los ángulos
+				GenerateCrossVertices(generator, value, x, y, z, customGeometry.SubsetAlphaTest);
+			}
 		}
 
-		// Token: 0x0600001E RID: 30 RVA: 0x00002468 File Offset: 0x00000668
+		// Método para generar vértices en forma de cruz (dos planos perpendiculares)
+		private void GenerateCrossVertices(BlockGeometryGenerator generator, int value, int x, int y, int z, TerrainGeometrySubset subset)
+		{
+			// Luz
+			int light = Terrain.ExtractLight(value);
+			float intensity = LightingManager.LightIntensityByLightValue[light];
+			Color color = Color.MultiplyColorOnlyNotSaturated(Color.White, intensity);
+
+			int textureSlot = this.GetFaceTextureSlot(4, value);
+			int textureSlotCount = this.GetTextureSlotCount(value);
+
+			// Coordenadas de textura (toda la imagen)
+			float u0 = (float)(textureSlot % textureSlotCount) / textureSlotCount;
+			float v0 = (float)(textureSlot / textureSlotCount) / textureSlotCount;
+			float u1 = u0 + 1.0f / textureSlotCount;
+			float v1 = v0 + 1.0f / textureSlotCount;
+
+			float halfSize = 0.45f;
+
+			// Plano en X (eje X, Z fijo en 0.5)
+			Vector3[] quadX = new Vector3[4];
+			quadX[0] = new Vector3(x + 0.5f - halfSize, y, z + 0.5f);
+			quadX[1] = new Vector3(x + 0.5f + halfSize, y, z + 0.5f);
+			quadX[2] = new Vector3(x + 0.5f + halfSize, y + 1f, z + 0.5f);
+			quadX[3] = new Vector3(x + 0.5f - halfSize, y + 1f, z + 0.5f);
+
+			// Plano en Z (eje Z, X fijo en 0.5)
+			Vector3[] quadZ = new Vector3[4];
+			quadZ[0] = new Vector3(x + 0.5f, y, z + 0.5f - halfSize);
+			quadZ[1] = new Vector3(x + 0.5f, y, z + 0.5f + halfSize);
+			quadZ[2] = new Vector3(x + 0.5f, y + 1f, z + 0.5f + halfSize);
+			quadZ[3] = new Vector3(x + 0.5f, y + 1f, z + 0.5f - halfSize);
+
+			// Función auxiliar para añadir un cuadrilátero con doble cara
+			void AddQuad(Vector3[] quad)
+			{
+				// Añadir los 4 vértices
+				int start = subset.Vertices.Count;
+				subset.Vertices.Count += 4;
+				var vertices = subset.Vertices.Array;
+
+				BlockGeometryGenerator.SetupVertex(quad[0].X, quad[0].Y, quad[0].Z, color, u0, v1, ref vertices[start]);
+				BlockGeometryGenerator.SetupVertex(quad[1].X, quad[1].Y, quad[1].Z, color, u1, v1, ref vertices[start + 1]);
+				BlockGeometryGenerator.SetupVertex(quad[2].X, quad[2].Y, quad[2].Z, color, u1, v0, ref vertices[start + 2]);
+				BlockGeometryGenerator.SetupVertex(quad[3].X, quad[3].Y, quad[3].Z, color, u0, v0, ref vertices[start + 3]);
+
+				// Índices para ambas caras
+				int idxStart = subset.Indices.Count;
+				subset.Indices.Count += 12;  // 2 caras * 2 triángulos * 3 índices = 12
+				var indices = subset.Indices.Array;
+
+				// Cara frontal (orden original)
+				indices[idxStart] = start;
+				indices[idxStart + 1] = start + 1;
+				indices[idxStart + 2] = start + 2;
+				indices[idxStart + 3] = start + 2;
+				indices[idxStart + 4] = start + 3;
+				indices[idxStart + 5] = start;
+
+				// Cara trasera (orden inverso)
+				indices[idxStart + 6] = start;
+				indices[idxStart + 7] = start + 3;
+				indices[idxStart + 8] = start + 2;
+				indices[idxStart + 9] = start + 2;
+				indices[idxStart + 10] = start + 1;
+				indices[idxStart + 11] = start;
+			}
+
+			AddQuad(quadX);
+			AddQuad(quadZ);
+		}
 		public override void DrawBlock(PrimitivesRenderer3D primitivesRenderer, int value, Color color, float size, ref Matrix matrix, DrawBlockEnvironmentData environmentData)
 		{
 			if (this.m_texture != null)
@@ -43,63 +116,64 @@ namespace Game
 			}
 		}
 
-		// Token: 0x0600001F RID: 31 RVA: 0x000024A0 File Offset: 0x000006A0
 		public override BlockPlacementData GetPlacementValue(SubsystemTerrain subsystemTerrain, ComponentMiner componentMiner, int value, TerrainRaycastResult raycastResult)
 		{
-			// Crear datos de colocación con la cara de la celda
 			BlockPlacementData placementData = new BlockPlacementData();
 			placementData.Value = value;
 			placementData.CellFace = raycastResult.CellFace;
 			return placementData;
 		}
 
-		// Token: 0x06000020 RID: 32 RVA: 0x000024CC File Offset: 0x000006CC
 		public override BoundingBox[] GetCustomCollisionBoxes(SubsystemTerrain terrain, int value)
 		{
-			// Sin cajas de colisión para bloques planos
 			return new BoundingBox[0];
 		}
 
-		// Token: 0x06000021 RID: 33 RVA: 0x000024D4 File Offset: 0x000006D4
 		public override BoundingBox[] GetCustomInteractionBoxes(SubsystemTerrain terrain, int value)
 		{
-			// Sin cajas de interacción para bloques planos
 			return new BoundingBox[0];
 		}
 
-		// Token: 0x06000022 RID: 34 RVA: 0x000024DC File Offset: 0x000006DC
 		public override bool IsFaceTransparent(SubsystemTerrain subsystemTerrain, int face, int value)
 		{
-			// Todas las caras son transparentes
 			return true;
 		}
 
-		// Token: 0x06000023 RID: 35 RVA: 0x000024E0 File Offset: 0x000006E0
 		public override bool ShouldGenerateFace(SubsystemTerrain subsystemTerrain, int face, int value, int neighborValue, int x, int y, int z)
 		{
-			// No generar caras para este bloque
 			return false;
 		}
 
-		// Token: 0x06000024 RID: 36 RVA: 0x000024E4 File Offset: 0x000006E4
 		public override int GetFaceTextureSlot(int face, int value)
 		{
-			// Todas las caras usan la misma textura
 			return 0;
 		}
 
-		// Token: 0x06000025 RID: 37 RVA: 0x000024E8 File Offset: 0x000006E8
 		public override void GetDropValues(SubsystemTerrain subsystemTerrain, int oldValue, int newValue, int toolLevel, List<BlockDropValue> dropValues, out bool showDebris)
 		{
-			// Sin gotas al destruir
 			showDebris = false;
 		}
 
-		// Token: 0x06000028 RID: 40 RVA: 0x000024FC File Offset: 0x000006FC
 		public override int GetTextureSlotCount(int value)
 		{
-			// Solo una textura
 			return 1;
+		}
+
+		public override bool IsCollidable_(int value)
+		{
+			return false;
+		}
+
+		public override bool IsTransparent_(int value)
+		{
+			return true;
+		}
+
+		public override float? Raycast(Ray3 ray, SubsystemTerrain subsystemTerrain, int value, bool useInteractionBoxes, out int collisionBoxIndex, out BoundingBox collisionBox)
+		{
+			collisionBoxIndex = 0;
+			collisionBox = default(BoundingBox);
+			return null;
 		}
 	}
 }
