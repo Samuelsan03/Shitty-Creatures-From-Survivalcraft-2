9de6bb873c4f2206be35bb8534d9b2004ae5fba1
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-04-03 18:07:17 -0500

    Update ComponentZombieAI2.cs

--- Funciones De Mierda/ComponentZombieAI2.cs
+++ Funciones De Mierda/ComponentZombieAI2.cs
@@ -149,8 +149,8 @@ namespace Game
 		private float m_throwableMinRange = 5f;      // AÑADIR
 		private float m_throwableMaxRange = 15.5f;     // AÑADIR
 
-		private float m_explosiveBoltMinDistance = 15f;
-		private float m_explosiveRepeatArrowMinDistance = 15f;
+		private float m_explosiveBoltMinDistance = 20f;
+		private float m_explosiveRepeatArrowMinDistance = 20f;
 
 		private float m_repeatCrossbowDrawTime = 0.3f;
 		private float m_repeatCrossbowTimeBetweenShots = 0.5f;
