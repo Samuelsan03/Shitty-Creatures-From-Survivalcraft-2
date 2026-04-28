19dff2901e7a41f5ef3666d3dc62b9628ca1497d
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-04-28 14:01:05 -0500

    Update ComponentZombieAI2.cs

--- ShittyCreatures/ComponentZombieAI2.cs
+++ ShittyCreatures/ComponentZombieAI2.cs
@@ -619,7 +619,7 @@ namespace Game
 
 		public void Update(float dt)
 		{
-			if (!CanUseInventory || m_componentCreature == null || m_componentCreature.ComponentHealth.Health <= 0f)
+			if (m_componentCreature == null || m_componentCreature.ComponentHealth.Health <= 0f)
 				return;
 
 			if (m_canEquipClothing && m_componentCreatureClothing != null && m_componentInventory != null)
@@ -701,6 +701,9 @@ namespace Game
 					}
 				}
 			}
+
+			if (!CanUseInventory)
+				return;
 
 			ComponentCreature target = m_componentChaseBehavior?.Target;
 
