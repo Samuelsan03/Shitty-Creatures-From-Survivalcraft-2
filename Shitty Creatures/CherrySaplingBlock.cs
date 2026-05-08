35ae6539cfd65b18526dcb6d4769df3049f5222d
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-05-08 08:11:42 -0500

    Add files via upload

---
+++ Shitty Creatures/CherrySaplingBlock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game
+{
+	public class CherrySaplingBlock : ShittyTexturesFlat
+	{
+		public static int Index;
+
+		public CherrySaplingBlock() : base("Textures/alimentos/arbol joven cerezo")
+		{
+		}
+
+		public override void Initialize()
+		{
+			base.Initialize();
+			Index = this.BlockIndex;
+		}
+
+		public override IEnumerable<int> GetCreativeValues()
+		{
+			yield return Terrain.MakeBlockValue(BlockIndex, 0, 0);
+		}
+	}
+}
