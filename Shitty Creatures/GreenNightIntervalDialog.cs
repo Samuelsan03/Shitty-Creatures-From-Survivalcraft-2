69e33c8b7dc77cf10eefcdce63e250f807ad0f4a
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-05-13 22:26:50 -0500

    Create GreenNightIntervalDialog.cs

---
+++ Shitty Creatures/GreenNightIntervalDialog.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Xml.Linq;
+using Engine;
+using Game;
+
+namespace Game
+{
+	public class GreenNightIntervalDialog : Dialog
+	{
+		private SubsystemGreenNightSky m_greenNightSky;
+		private ComponentPlayer m_player;
+		private int m_selectedDays;
+		private bool m_isClosing;
+
+		private BevelledButtonWidget m_daysButton;
+		private BevelledButtonWidget m_okButton;
+		private BevelledButtonWidget m_cancelButton;
+		private LabelWidget m_descriptionLabel;
+
+		private readonly int[] m_options = { 4, 8, 12, 16 };
+
+		private string GetText(int key)
+		{
+			return LanguageControl.GetContentWidgets("GreenNightIntervalDialog", key.ToString());
+		}
+
+		private string GetDescription(int days)
+		{
+			if (days == 4) return GetText(6);
+			if (days == 8) return GetText(7);
+			if (days == 12) return GetText(8);
+			return GetText(9);
+		}
+
+		public GreenNightIntervalDialog(SubsystemGreenNightSky greenNightSky, ComponentPlayer player)
+		{
+			m_greenNightSky = greenNightSky;
+			m_player = player;
+			m_selectedDays = m_greenNightSky.GreenNightIntervalDays;
+
+			XElement node = ContentManager.Get<XElement>("Dialogs/GreenNightIntervalDialog");
+			LoadContents(null, node);
+
+			m_daysButton = Children.Find<BevelledButtonWidget>("GreenNightIntervalDialog.DaysButton", true);
+			m_descriptionLabel = Children.Find<LabelWidget>("GreenNightIntervalDialog.DescriptionLabel", true);
+			m_okButton = Children.Find<BevelledButtonWidget>("GreenNightIntervalDialog.OkButton", true);
+			m_cancelButton = Children.Find<BevelledButtonWidget>("GreenNightIntervalDialog.CancelButton", true);
+
+			UpdateDaysButton();
+			UpdateDescription(m_selectedDays);
+		}
+
+		private void UpdateDaysButton()
+		{
+			string format = GetText(12);
+			m_daysButton.Text = string.Format(format, m_selectedDays);
+		}
+
+		private void UpdateDescription(int days)
+		{
+			m_descriptionLabel.Text = GetDescription(days);
+			m_descriptionLabel.Color = new Color(255, 165, 0);
+		}
+
+		public override void Update()
+		{
+			if (m_isClosing) return;
+
+			if (m_daysButton.IsClicked)
+			{
+				CycleDays();
+			}
+			else if (m_okButton.IsClicked)
+			{
+				Accept();
+			}
+			else if (m_cancelButton.IsClicked)
+			{
+				Cancel();
+			}
+
+			base.Update();
+		}
+
+		private void CycleDays()
+		{
+			int index = Array.IndexOf(m_options, m_selectedDays);
+			index = (index + 1) % m_options.Length;
+			m_selectedDays = m_options[index];
+			UpdateDaysButton();
+			UpdateDescription(m_selectedDays);
+		}
+
+		private void Accept()
+		{
+			if (m_isClosing) return;
+			m_isClosing = true;
+
+			m_greenNightSky.GreenNightIntervalDays = m_selectedDays;
+
+			string message = string.Format(GetText(11), m_selectedDays);
+			m_player.ComponentGui.DisplaySmallMessage(message, Color.White, false, true);
+
+			DialogsManager.HideDialog(this);
+		}
+
+		private void Cancel()
+		{
+			if (m_isClosing) return;
+			m_isClosing = true;
+
+			m_greenNightSky.GreenNightIntervalDays = 4;
+
+			string message = string.Format(GetText(11), 4);
+			m_player.ComponentGui.DisplaySmallMessage(message, Color.White, false, true);
+
+			DialogsManager.HideDialog(this);
+		}
+	}
+}
