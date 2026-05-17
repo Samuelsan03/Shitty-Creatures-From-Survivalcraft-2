f3fc7d6fd215794fb70f78f82c1212f5b3291220
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-05-17 12:19:08 -0500

    Update and rename ShittySeedsBlock.cs to BlueberrySeedBlock.cs

--- Shitty Creatures/BlueberrySeedBlock.cs
+++ Shitty Creatures/BlueberrySeedBlock.cs
@@ -2,51 +2,42 @@ using System;
 using System.Collections.Generic;
 using Engine;
 using Engine.Graphics;
+using Game;
 
 namespace Game
 {
-	public class ShittySeedsBlock : FlatBlock
+	public class BlueberrySeedBlock : FlatBlock
 	{
 		public static int Index = 431;
 
 		public override IEnumerable<int> GetCreativeValues()
 		{
-			List<int> list = new List<int>();
-			foreach (int data in EnumUtils.GetEnumValues<ShittySeedsBlock.SeedType>())
-			{
-				list.Add(Terrain.MakeBlockValue(Index, 0, data));
-			}
-			return list;
+			return new int[] { Terrain.MakeBlockValue(Index, 0, 0) };
 		}
 
 		public override int GetFaceTextureSlot(int face, int value)
 		{
-			return 75;
+			// Usar una ranura de textura específica para la semilla de arándano
+			return 75; // Puedes cambiar si existe otra textura
 		}
 
 		public override BlockPlacementData GetPlacementValue(SubsystemTerrain subsystemTerrain, ComponentMiner componentMiner, int value, TerrainRaycastResult raycastResult)
 		{
 			BlockPlacementData result = default(BlockPlacementData);
 			result.CellFace = raycastResult.CellFace;
-			if (raycastResult.CellFace.Face == 4)
+			if (raycastResult.CellFace.Face == 4) // Solo colocar en la cara superior
 			{
-				if (Terrain.ExtractData(value) == 0)
-				{
-					result.Value = Terrain.MakeBlockValue(BlueberryBushBlock.Index, 0, 0);
-				}
+				// Colocar un arbusto de arándano (BlueberryBushBlock) con tamaño 0 (pequeño)
+				result.Value = Terrain.MakeBlockValue(422, 0, 0);
 			}
 			return result;
 		}
 
 		public override void DrawBlock(PrimitivesRenderer3D primitivesRenderer, int value, Color color, float size, ref Matrix matrix, DrawBlockEnvironmentData environmentData)
 		{
-			color *= new Color(50, 60, 120);
+			// Tinte opcional para la semilla
+			color *= new Color(70, 90, 150); // Color azulado
 			BlocksManager.DrawFlatOrImageExtrusionBlock(primitivesRenderer, value, size, ref matrix, null, color, false, environmentData);
-		}
-
-		public enum SeedType
-		{
-			Blueberry
 		}
 	}
 }
