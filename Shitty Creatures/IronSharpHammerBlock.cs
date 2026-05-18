de6e92cfe07f362ec9c6558dc1259a84bb8b70b8
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-05-18 16:54:14 -0500

    Update IronSharpHammerBlock.cs

--- Shitty Creatures/IronSharpHammerBlock.cs
+++ Shitty Creatures/IronSharpHammerBlock.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Game
 {
@@ -9,19 +9,8 @@ namespace Game
 		public IronSharpHammerBlock() : base(47, 63)
 		{
 		}
-		public override string GetDisplayName(SubsystemTerrain subsystemTerrain, int value)
-		{
-			string displayName = LanguageControl.GetBlock("IronSharpHammerBlock", "DisplayName");
-				return string.IsNullOrEmpty(displayName) ? "Iron Sharp Hammer" : displayName;
-		}
-
-		public override string GetDescription(int value)
-		{
-			string description = LanguageControl.GetBlock("IronSharpHammerBlock", "Description");
-			return string.IsNullOrEmpty(description) ? "The Iron Sharp Hammer is similar to the iron machete, but slightly more lethal and a bit more durable. While it shares the same brutal design, its enhanced endurance and sharper impact make it a superior choice for those seeking a weapon that balances resilience with deadly efficiency." : description;
-		}
 
 		// Token: 0x040000EF RID: 239
-		public static int Index = 334; // Cambia el índice según lo necesites
+		public static int Index = 460; // Cambia el índice según lo necesites
 	}
 }
