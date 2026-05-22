af898e5d4a5f6757ecbafad2d450483ad610a9af
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-05-22 18:07:38 -0500

    Add files via upload

--- Shitty Creatures/MendozaBulletBlock.cs
+++ Shitty Creatures/MendozaBulletBlock.cs
@@ -1,34 +1,14 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using Engine;
 using Engine.Graphics;
 
 namespace Game
 {
-	public class MendozaBulletBlock : Block
+	public class MendozaBulletBlock : ShittyTexturesFlat
 	{
-		public override void Initialize()
+		public MendozaBulletBlock() : base("Textures/Armas De Mierda/Mendoza bullet")
 		{
-			Model model = ContentManager.Get<Model>("Models/Armas De Porqueria/Plate", null);
-			Matrix boneAbsoluteTransform = BlockMesh.GetBoneAbsoluteTransform(model.FindMesh("Plate", true).ParentBone);
-			this.m_standaloneBlockMesh.AppendModelMeshPart(model.FindMesh("Plate", true).MeshParts[0], boneAbsoluteTransform * Matrix.CreateTranslation(0f, -0.1f, 0f) * Matrix.CreateRotationX(1.5707f), false, false, true, false, Color.White);
-			base.Initialize();
+			this.BlockIndex = 485;
 		}
-
-		public override void GenerateTerrainVertices(BlockGeometryGenerator generator, TerrainGeometry geometry, int value, int x, int y, int z)
-		{
-			generator.GenerateShadedMeshVertices(this, x, y, z, this.m_standaloneBlockMesh, Color.White, null, null, geometry.SubsetOpaque);
-		}
-
-		public override void DrawBlock(PrimitivesRenderer3D primitivesRenderer, int value, Color color, float size, ref Matrix matrix, DrawBlockEnvironmentData environmentData)
-		{
-			BlocksManager.DrawMeshBlock(primitivesRenderer, this.m_standaloneBlockMesh, this.texture, color, 1.9f * size, ref matrix, environmentData);
-		}
-
-		public const int Index = 485;
-
-		private BlockMesh m_standaloneBlockMesh = new BlockMesh();
-
-		public Texture2D texture = ContentManager.Get<Texture2D>("Textures/Armas De Mierda/Mendoza bullet", null);
 	}
 }
