a7c55bf7e44eb2b3e4f82f57e48e4d2c0799f5b8
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-06-27 09:00:36 -0500

    Update GunFireParticleSystem.cs

--- Shitty Creatures/GunFireParticleSystem.cs
+++ Shitty Creatures/GunFireParticleSystem.cs
@@ -1,175 +1,123 @@
-﻿using System;
-using System.Runtime.CompilerServices;
+using System;
 using Engine;
 using Engine.Graphics;
 
 namespace Game
 {
-	// Token: 0x02000086 RID: 134
-	public class GunFireParticleSystem : ParticleSystem<GunFireParticleSystem.Particle>
+	// Token: 0x02000100 RID: 256
+	public class GunFireParticleSystem : ParticleSystem<GunSmokeParticleSystem.Particle>
 	{
-		// Token: 0x170000B6 RID: 182
-		// (get) Token: 0x0600065C RID: 1628 RVA: 0x0004EA1F File Offset: 0x0004CC1F
-		// (set) Token: 0x0600065D RID: 1629 RVA: 0x0004EA27 File Offset: 0x0004CC27
-		public bool IsStopped { get; set; }
-
-		// Token: 0x0600065E RID: 1630 RVA: 0x0004EA30 File Offset: 0x0004CC30
-		public GunFireParticleSystem(Vector3 position, Vector3 direction, float maxVisibilityDistance) : base(25)
+		// Token: 0x060006DF RID: 1759 RVA: 0x0003A434 File Offset: 0x00038634
+		public GunFireParticleSystem(SubsystemTerrain terrain, Vector3 position, Vector3 direction, float scale) : base(50)
 		{
-			this.m_position = position;
-			this.m_direction = direction;
-			this.m_maxVisibilityDistance = maxVisibilityDistance;
-			this.m_isOneShot = true;
-			base.Texture = ContentManager.Get<Texture2D>("Textures/Gui/NewFireParticle");
+			base.Texture = ContentManager.Get<Texture2D>("Textures/Gui/NewFireParticle", null);
 			base.TextureSlotsCount = 3;
+			this.m_position = position;
+			this.m_direction = Vector3.Normalize(direction);
+			int num = Terrain.ToCell(position.X);
+			int num2 = Terrain.ToCell(position.Y);
+			int num3 = Terrain.ToCell(position.Z);
+			int x = 0;
+			x = MathUtils.Max(x, terrain.Terrain.GetCellLight(num, num2 + 1, num3));
+			float num4 = (float)MathUtils.Max(MathUtils.Max(MathUtils.Max(MathUtils.Max(MathUtils.Max(MathUtils.Max(0, terrain.Terrain.GetCellLight(num + 1, num2, num3)), terrain.Terrain.GetCellLight(num - 1, num2, num3)), terrain.Terrain.GetCellLight(num, num2 + 1, num3)), terrain.Terrain.GetCellLight(num, num2 - 1, num3)), terrain.Terrain.GetCellLight(num, num2, num3 + 1)), terrain.Terrain.GetCellLight(num, num2, num2 - 1));
+			this.m_color = new Color(num4, num4, num4);
 		}
 
-		// Token: 0x0600065F RID: 1631 RVA: 0x0004EA88 File Offset: 0x0004CC88
-		public GunFireParticleSystem(SubsystemTerrain subsystemTerrain, Vector3 position, Vector3 direction) : base(25)
+		// Token: 0x060006E0 RID: 1760 RVA: 0x0003A550 File Offset: 0x00038750
+		public GunFireParticleSystem(SubsystemTerrain terrain, Vector3 position, Vector3 direction) : base(50)
 		{
-			this.subsystemTerrain = subsystemTerrain;
-			this.m_position = position;
-			this.m_direction = direction;
-			this.m_isOneShot = true;
-			base.Texture = ContentManager.Get<Texture2D>("Textures/Gui/NewFireParticle");
+			base.Texture = ContentManager.Get<Texture2D>("Textures/Gui/NewFireParticle", null);
 			base.TextureSlotsCount = 3;
-			this.m_maxVisibilityDistance = 50f;
+			this.m_position = position;
+			this.m_direction = Vector3.Normalize(direction);
+			int num = Terrain.ToCell(position.X);
+			int num2 = Terrain.ToCell(position.Y);
+			int num3 = Terrain.ToCell(position.Z);
+			int x = 0;
+			x = MathUtils.Max(x, terrain.Terrain.GetCellLight(num + 1, num2, num3));
+			x = MathUtils.Max(x, terrain.Terrain.GetCellLight(num - 1, num2, num3));
+			x = MathUtils.Max(x, terrain.Terrain.GetCellLight(num, num2 + 1, num3));
+			x = MathUtils.Max(x, terrain.Terrain.GetCellLight(num, num2 - 1, num3));
+			x = MathUtils.Max(x, terrain.Terrain.GetCellLight(num, num2, num3 + 1));
+			x = MathUtils.Max(x, terrain.Terrain.GetCellLight(num, num2, num3 - 1));
+			this.m_scale = 0.1f;
 		}
 
-		// Token: 0x06000660 RID: 1632 RVA: 0x0004EAEC File Offset: 0x0004CCEC
+		// Token: 0x060006E1 RID: 1761 RVA: 0x0003A658 File Offset: 0x00038858
 		public override bool Simulate(float dt)
 		{
-			this.m_age += dt;
-			bool flag = false;
-			bool flag2 = this.m_visible || this.m_age < 3f;
-			bool flag3 = flag2;
-			if (flag3)
+			this.m_time += dt;
+			float num = MathUtils.Lerp(100f, 10f, MathUtils.Saturate(2f * this.m_time / 0.5f));
+			float s = MathUtils.Pow(0.01f, dt);
+			float s2 = MathUtils.Lerp(10f, 0f, MathUtils.Saturate(2f * this.m_time / 0.5f));
+			Vector3 v = new Vector3(1f, 1f, 0.5f);
+			if (this.m_time < 0.1f)
 			{
-				bool flag4 = this.m_age < 0.3f;
-				bool flag5 = flag4;
-				if (flag5)
-				{
-					this.m_toGenerate += 12f * dt;
-				}
-				for (int i = 0; i < base.Particles.Length; i++)
-				{
-					GunFireParticleSystem.Particle particle = base.Particles[i];
-					bool isActive = particle.IsActive;
-					bool flag6 = isActive;
-					if (flag6)
-					{
-						flag = true;
-						particle.Time += dt;
-						particle.TimeToLive -= dt;
-						bool flag7 = particle.TimeToLive > 0f;
-						bool flag8 = flag7;
-						if (flag8)
-						{
-							particle.Position += particle.Velocity * dt;
-							particle.Velocity *= 0.92f;
-							GunFireParticleSystem.Particle particle2 = particle;
-							particle2.Velocity.Y = particle2.Velocity.Y + 0.5f * dt;
-							particle.TextureSlot = (int)MathUtils.Min(9f * particle.Time / 1.2f, 8f);
-							float num = particle.Time / particle.TimeToLive;
-							float num2 = MathUtils.Max(1f - num * num, 0f);
-							particle.Size = new Vector2(0.08f * num2);
-							particle.Color = Color.White * num2;
-						}
-						else
-						{
-							particle.IsActive = false;
-						}
-					}
-					else
-					{
-						bool flag9 = this.m_toGenerate >= 1f && this.m_age < 0.4f;
-						bool flag10 = flag9;
-						if (flag10)
-						{
-							particle.IsActive = true;
-							particle.Position = this.m_position;
-							particle.Color = Color.White;
-							particle.Size = new Vector2(0.08f);
-							Vector3 v = this.m_direction * this.m_random.Float(2f, 4f);
-							Vector3 v2 = new Vector3(this.m_random.Float(-0.5f, 0.5f), this.m_random.Float(-0.2f, 0.6f), this.m_random.Float(-0.5f, 0.5f));
-							particle.Velocity = v + v2;
-							particle.Time = 0f;
-							particle.TimeToLive = this.m_random.Float(0.8f, 1.5f);
-							particle.FlipX = this.m_random.Bool();
-							particle.FlipY = this.m_random.Bool();
-							this.m_toGenerate -= 1f;
-						}
-					}
-				}
-				this.m_toGenerate = MathUtils.Remainder(this.m_toGenerate, 1f);
-			}
-			this.m_visible = false;
-			bool flag11 = this.m_age > 2.5f && !flag;
-			bool flag12 = flag11;
-			bool result;
-			if (flag12)
-			{
-				result = true;
+				this.m_toGenerate += num * dt;
 			}
 			else
 			{
-				bool flag13 = this.m_age > 5f;
-				result = flag13;
+				this.m_toGenerate = 0f;
 			}
-			return result;
-		}
-
-		// Token: 0x06000661 RID: 1633 RVA: 0x0004EE50 File Offset: 0x0004D050
-		public override void Draw(Camera camera)
-		{
-			float num = Vector3.Dot(this.m_position - camera.ViewPosition, camera.ViewDirection);
-			bool flag = num > -0.5f && num <= this.m_maxVisibilityDistance && Vector3.DistanceSquared(this.m_position, camera.ViewPosition) <= this.m_maxVisibilityDistance * this.m_maxVisibilityDistance;
-			bool flag2 = flag;
-			if (flag2)
+			bool flag = false;
+			for (int i = 0; i < base.Particles.Length; i++)
 			{
-				this.m_visible = true;
-				base.Draw(camera);
+				GunSmokeParticleSystem.Particle particle = base.Particles[i];
+				if (particle.IsActive)
+				{
+					flag = true;
+					particle.Time += dt;
+					if (particle.Time <= particle.Duration)
+					{
+						particle.Position += particle.Velocity * dt * 0.25f;
+						particle.Velocity *= s;
+						particle.Velocity += v * dt;
+						particle.TextureSlot = (int)MathUtils.Min(20f * particle.Time / particle.Duration, 8f);
+						particle.Size = new Vector2(this.m_scale);
+					}
+					else
+					{
+						particle.IsActive = false;
+					}
+				}
+				else if (this.m_toGenerate >= 1f)
+				{
+					particle.IsActive = true;
+					Vector3 v2 = this.m_random.Vector3(0f, 0.5f);
+					particle.Position = this.m_position + 0.3f * v2;
+					particle.Color = Color.White;
+					particle.Velocity = s2 * (this.m_direction + this.m_random.Vector3(0f, 0.1f)) + 2.5f * v2;
+					particle.Size = Vector2.Zero;
+					particle.Time = 0f;
+					particle.Duration = this.m_random.Float(0.1f, 1f);
+					particle.FlipX = this.m_random.Bool();
+					particle.FlipY = this.m_random.Bool();
+					this.m_toGenerate -= 1f;
+				}
 			}
+			this.m_toGenerate = MathUtils.Remainder(this.m_toGenerate, 1f);
+			return !flag && this.m_time >= 0.5f;
 		}
 
-		// Token: 0x040005F5 RID: 1525
-		public Random m_random = new Random();
+		// Token: 0x04000446 RID: 1094
+		public Game.Random m_random = new Game.Random();
 
-		// Token: 0x040005F6 RID: 1526
-		public Vector3 m_position;
+		// Token: 0x04000447 RID: 1095
+		public float m_time;
 
-		// Token: 0x040005F7 RID: 1527
-		public Vector3 m_direction;
-
-		// Token: 0x040005F8 RID: 1528
+		// Token: 0x04000448 RID: 1096
 		public float m_toGenerate;
 
-		// Token: 0x040005F9 RID: 1529
-		public bool m_visible;
+		// Token: 0x04000449 RID: 1097
+		public Vector3 m_position;
 
-		// Token: 0x040005FA RID: 1530
-		public float m_maxVisibilityDistance;
+		// Token: 0x0400044A RID: 1098
+		public Vector3 m_direction;
 
-		// Token: 0x040005FB RID: 1531
-		public float m_age;
+		// Token: 0x0400044B RID: 1099
+		public float m_scale;
 
-		// Token: 0x040005FC RID: 1532
-		public bool m_isOneShot;
-
-		// Token: 0x040005FD RID: 1533
-		private SubsystemTerrain subsystemTerrain;
-
-		// Token: 0x0200017A RID: 378
-		public class Particle : Game.Particle
-		{
-			// Token: 0x04000A64 RID: 2660
-			public float Time;
-
-			// Token: 0x04000A65 RID: 2661
-			public float TimeToLive;
-
-			// Token: 0x04000A66 RID: 2662
-			public Vector3 Velocity;
-		}
+		// Token: 0x0400044C RID: 1100
+		public Color m_color;
 	}
 }
