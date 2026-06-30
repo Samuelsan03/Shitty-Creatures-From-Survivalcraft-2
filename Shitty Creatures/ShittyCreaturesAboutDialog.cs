615189ec4e6b42b52ddea4c5d89346a9c8ff22ed
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-06-30 18:04:53 -0500

    Refactor ShittyCreaturesAboutDialog for clarity
    Removed unused variables and cleaned up the constructor. Simplified the Update method by removing unnecessary scroll position checks.

--- Shitty Creatures/ShittyCreaturesAboutDialog.cs
+++ Shitty Creatures/ShittyCreaturesAboutDialog.cs
@@ -6,97 +6,88 @@ using Game;
 
 namespace Game
 {
-	public class ShittyCreaturesAboutDialog : Dialog
-	{
-		public LabelWidget m_titleLabel;
+    public class ShittyCreaturesAboutDialog : Dialog
+    {
+        public LabelWidget m_titleLabel;
 
-		public LabelWidget m_contentLabel;
+        public LabelWidget m_contentLabel;
 
-		public LabelWidget m_creatorTitleLabel;
+        public LabelWidget m_creatorTitleLabel;
 
-		public LabelWidget m_creatorNameLabel;
+        public LabelWidget m_creatorNameLabel;
 
-		public LabelWidget m_socialTitleLabel;
+        public LabelWidget m_socialTitleLabel;
 
-		public LabelWidget m_youtubeLabel;
+        public LabelWidget m_youtubeLabel;
 
-		public LabelWidget m_twitterLabel;
+        public LabelWidget m_twitterLabel;
 
-		public LabelWidget m_discordLabel;
+        public LabelWidget m_discordLabel;
 
-		public LabelWidget m_biliBiliLabel;
+        public LabelWidget m_biliBiliLabel;
 
-		public LabelWidget m_modPageLabel;
+        public LabelWidget m_modPageLabel;
 
-		public LabelWidget m_finalMessageLabel;
+        public LabelWidget m_finalMessageLabel;
 
-		public ButtonWidget m_okButton;
+        public ButtonWidget m_okButton;
 
-		public LabelWidget m_buttonLabel;
+        public LabelWidget m_buttonLabel;
 
-		public ScrollPanelWidget m_scrollPanel;
+        public ShittyCreaturesAboutDialog()
+        {
+            XElement node = ContentManager.Get<XElement>("Dialogs/ShittyCreaturesAboutDialog");
+            this.LoadContents(this, node);
+            this.m_okButton = this.Children.Find<ButtonWidget>("OkButton", true);
+            this.m_buttonLabel = this.Children.Find<LabelWidget>("ButtonLabel", true);
+            this.m_titleLabel = this.Children.Find<LabelWidget>("Title", true);
+            this.m_contentLabel = this.Children.Find<LabelWidget>("Content", true);
+            this.m_creatorTitleLabel = this.Children.Find<LabelWidget>("CreatorTitle", true);
+            this.m_creatorNameLabel = this.Children.Find<LabelWidget>("CreatorName", true);
+            this.m_socialTitleLabel = this.Children.Find<LabelWidget>("SocialTitle", true);
+            this.m_youtubeLabel = this.Children.Find<LabelWidget>("YoutubeLabel", true);
+            this.m_twitterLabel = this.Children.Find<LabelWidget>("TwitterLabel", true);
+            this.m_discordLabel = this.Children.Find<LabelWidget>("DiscordLabel", true);
+            this.m_biliBiliLabel = this.Children.Find<LabelWidget>("BiliBiliLabel", true);
+            this.m_modPageLabel = this.Children.Find<LabelWidget>("ModPageLabel", true);
+            this.m_finalMessageLabel = this.Children.Find<LabelWidget>("FinalMessage", true);
+            this.m_buttonLabel.Text = LanguageControl.Ok;
+            ApplyTranslations();
+        }
 
-		public ShittyCreaturesAboutDialog()
-		{
-			XElement node = ContentManager.Get<XElement>("Dialogs/ShittyCreaturesAboutDialog");
-			this.LoadContents(this, node);
-			this.m_okButton = this.Children.Find<ButtonWidget>("OkButton", true);
-			this.m_buttonLabel = this.Children.Find<LabelWidget>("ButtonLabel", true);
-			this.m_titleLabel = this.Children.Find<LabelWidget>("Title", true);
-			this.m_contentLabel = this.Children.Find<LabelWidget>("Content", true);
-			this.m_creatorTitleLabel = this.Children.Find<LabelWidget>("CreatorTitle", true);
-			this.m_creatorNameLabel = this.Children.Find<LabelWidget>("CreatorName", true);
-			this.m_socialTitleLabel = this.Children.Find<LabelWidget>("SocialTitle", true);
-			this.m_youtubeLabel = this.Children.Find<LabelWidget>("YoutubeLabel", true);
-			this.m_twitterLabel = this.Children.Find<LabelWidget>("TwitterLabel", true);
-			this.m_discordLabel = this.Children.Find<LabelWidget>("DiscordLabel", true);
-			this.m_biliBiliLabel = this.Children.Find<LabelWidget>("BiliBiliLabel", true);
-			this.m_modPageLabel = this.Children.Find<LabelWidget>("ModPageLabel", true);
-			this.m_finalMessageLabel = this.Children.Find<LabelWidget>("FinalMessage", true);
-			this.m_scrollPanel = this.Children.Find<ScrollPanelWidget>("ScrollPanel", true);
-			this.m_buttonLabel.Text = LanguageControl.Ok;
-			this.m_okButton.IsVisible = false;
-			ApplyTranslations();
-		}
+        private void ApplyTranslations()
+        {
+            SetLabelText(m_titleLabel, "ShittyCreaturesAbout", "Title");
+            SetLabelText(m_contentLabel, "ShittyCreaturesAbout", "Description");
+            SetLabelText(m_creatorTitleLabel, "ShittyCreaturesAbout", "CreatorTitle");
+            SetLabelText(m_creatorNameLabel, "ShittyCreaturesAbout", "CreatorName");
+            SetLabelText(m_socialTitleLabel, "ShittyCreaturesAbout", "SocialTitle");
+            SetLabelText(m_youtubeLabel, "ShittyCreaturesAbout", "YoutubeLabel");
+            SetLabelText(m_twitterLabel, "ShittyCreaturesAbout", "TwitterLabel");
+            SetLabelText(m_discordLabel, "ShittyCreaturesAbout", "DiscordLabel");
+            SetLabelText(m_biliBiliLabel, "ShittyCreaturesAbout", "BiliBiliLabel");
+            SetLabelText(m_modPageLabel, "ShittyCreaturesAbout", "ModPageLabel");
+            SetLabelText(m_finalMessageLabel, "ShittyCreaturesAbout", "FinalMessage");
+        }
 
-		private void ApplyTranslations()
-		{
-			SetLabelText(m_titleLabel, "ShittyCreaturesAbout", "Title");
-			SetLabelText(m_contentLabel, "ShittyCreaturesAbout", "Description");
-			SetLabelText(m_creatorTitleLabel, "ShittyCreaturesAbout", "CreatorTitle");
-			SetLabelText(m_creatorNameLabel, "ShittyCreaturesAbout", "CreatorName");
-			SetLabelText(m_socialTitleLabel, "ShittyCreaturesAbout", "SocialTitle");
-			SetLabelText(m_youtubeLabel, "ShittyCreaturesAbout", "YoutubeLabel");
-			SetLabelText(m_twitterLabel, "ShittyCreaturesAbout", "TwitterLabel");
-			SetLabelText(m_discordLabel, "ShittyCreaturesAbout", "DiscordLabel");
-			SetLabelText(m_biliBiliLabel, "ShittyCreaturesAbout", "BiliBiliLabel");
-			SetLabelText(m_modPageLabel, "ShittyCreaturesAbout", "ModPageLabel");
-			SetLabelText(m_finalMessageLabel, "ShittyCreaturesAbout", "FinalMessage");
-		}
+        private void SetLabelText(LabelWidget label, string category, string key)
+        {
+            if (label != null)
+            {
+                string translation = LanguageControl.Get(new string[] { category, key });
+                if (!string.IsNullOrEmpty(translation) && translation != $"{category}:{key}")
+                {
+                    label.Text = translation;
+                }
+            }
+        }
 
-		private void SetLabelText(LabelWidget label, string category, string key)
-		{
-			if (label != null)
-			{
-				string translation = LanguageControl.Get(new string[] { category, key });
-				if (!string.IsNullOrEmpty(translation) && translation != $"{category}:{key}")
-				{
-					label.Text = translation;
-				}
-			}
-		}
-
-		public override void Update()
-		{
-			float num = MathUtils.Max(this.m_scrollPanel.m_scrollAreaLength - this.m_scrollPanel.ActualSize.Y, 0f);
-			if (this.m_scrollPanel.ScrollPosition >= num * 0.8f && this.m_scrollPanel.m_scrollAreaLength != 0f)
-			{
-				this.m_okButton.IsVisible = true;
-			}
-			if (this.m_okButton.IsClicked)
-			{
-				DialogsManager.HideDialog(this);
-			}
-		}
-	}
+        public override void Update()
+        {
+            if (this.m_okButton.IsClicked)
+            {
+                DialogsManager.HideDialog(this);
+            }
+        }
+    }
 }
