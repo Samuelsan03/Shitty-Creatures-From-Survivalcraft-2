4aaa609ea20863102bd19c5aa498ec2a90151cf2
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-07-03 21:16:14 -0500

    Create AnimalInfectedSpawnEggBlock.cs

---
+++ Shitty Creatures/AnimalInfectedSpawnEggBlock.cs
@@ -0,0 +1,48 @@
+using System;
+using Engine;
+using Engine.Graphics;
+using TemplatesDatabase;
+using Game;
+
+namespace Game
+{
+	public class AnimalInfectedSpawnEggBlock : Block
+	{
+		public const int Index = 648; // Siguiente índice disponible
+		private const float Scale = 2f;
+
+		private BlockMesh m_blockMesh;
+		private Texture2D m_alertTexture;
+
+		public override void Initialize()
+		{
+			Model model = ContentManager.Get<Model>("Models/Egg");
+			Matrix boneTransform = BlockMesh.GetBoneAbsoluteTransform(model.FindMesh("Egg", true).ParentBone);
+			m_alertTexture = ContentManager.Get<Texture2D>("Textures/alerta", throwOnNotFound: true);
+			m_blockMesh = new BlockMesh();
+			m_blockMesh.AppendModelMeshPart(
+				model.FindMesh("Egg", true).MeshParts[0],
+				boneTransform,
+				false, false, false, false,
+				Color.Red // Color distintivo para huevos de animales
+			);
+			base.Initialize();
+		}
+
+		public override string GetCategory(int value)
+		{
+			return "Spawner Eggs";
+		}
+
+		public override void GenerateTerrainVertices(BlockGeometryGenerator generator, TerrainGeometry geometry, int value, int x, int y, int z) { }
+
+		public override void DrawBlock(PrimitivesRenderer3D primitivesRenderer, int value, Color color, float size, ref Matrix matrix, DrawBlockEnvironmentData environmentData)
+		{
+			float scaledSize = size * Scale;
+			if (m_alertTexture != null)
+				BlocksManager.DrawMeshBlock(primitivesRenderer, m_blockMesh, m_alertTexture, color, scaledSize, ref matrix, environmentData);
+			else
+				BlocksManager.DrawMeshBlock(primitivesRenderer, m_blockMesh, color, scaledSize, ref matrix, environmentData);
+		}
+	}
+}
