79d4fce971f0f2a28e199ec5eaead6276e0eb6ff
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-07-06 19:43:56 -0500

    Update SubsystemFreezingSnowballBlockBehavior.cs

--- Shitty Creatures/SubsystemFreezingSnowballBlockBehavior.cs
+++ Shitty Creatures/SubsystemFreezingSnowballBlockBehavior.cs
@@ -30,35 +30,13 @@ namespace Game
 
 		public override void OnFiredAsProjectile(Projectile projectile)
 		{
-			// La bola de nieve tiene escala 2.5f. Ajustamos el tamaño de la estela para que coincida.
-			// Nube principal que envuelve la bola
-			var particleSystem = new FreezingTrailParticleSystem(120, 1.2f, float.MaxValue, new Color(200, 240, 255, 220));
-			m_subsystemProjectiles.AddTrail(projectile, Vector3.Zero, particleSystem);
-
-			// Nube secundaria ligeramente más pequeña y desplazada para dar profundidad
-			var particleSystem2 = new FreezingTrailParticleSystem(80, 0.8f, float.MaxValue, new Color(180, 220, 255, 200));
-			m_subsystemProjectiles.AddTrail(projectile, new Vector3(0f, 0.05f, 0f), particleSystem2);
-
-			var trails = new List<FreezingTrailParticleSystem> { particleSystem, particleSystem2 };
-			m_projectileTrails[projectile] = trails;
-
+			m_subsystemProjectiles.AddTrail(projectile, Vector3.Zero, new FreezingTrailParticleSystem(120, 1.2f, float.MaxValue, Color.White));
 			projectile.ProjectileStoppedAction = ProjectileStoppedAction.Disappear;
-
 			m_subsystemAudio.PlaySound("Audio/Throw", 1.0f, m_random.Float(-0.2f, 0.2f), projectile.Position, 2f, true);
 		}
 
 		public override bool OnHitAsProjectile(CellFace? cellFace, ComponentBody componentBody, WorldItem worldItem)
 		{
-			Projectile projectile = worldItem as Projectile;
-			if (projectile != null && m_projectileTrails.TryGetValue(projectile, out List<FreezingTrailParticleSystem> trails))
-			{
-				foreach (var trail in trails)
-				{
-					trail.IsStopped = true;
-				}
-				m_projectileTrails.Remove(projectile);
-			}
-
 			if (componentBody != null)
 			{
 				ComponentCreature creature = componentBody.Entity.FindComponent<ComponentCreature>();
