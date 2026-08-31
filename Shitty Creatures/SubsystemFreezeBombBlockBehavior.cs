c4c5f00bcb7510f820196954f09028aa55941f96
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-08-31 11:02:51 -0500

    Refactor properties and access modifiers in FreezeBomb behavior

--- Shitty Creatures/SubsystemFreezeBombBlockBehavior.cs
+++ Shitty Creatures/SubsystemFreezeBombBlockBehavior.cs
@@ -1,7 +1,7 @@
-// SubsystemFreezeBombBlockBehavior.cs
 using System;
 using System.Collections.Generic;
 using Engine;
+using Engine.Graphics;
 using GameEntitySystem;
 using TemplatesDatabase;
 
@@ -9,112 +9,111 @@ namespace Game
 {
 	public class SubsystemFreezeBombBlockBehavior : SubsystemBlockBehavior, IUpdateable
 	{
-		public override int[] HandledBlocks => new int[] { FreezeBombBlock.Index };
-		public UpdateOrder UpdateOrder => UpdateOrder.Default;
+		public override int[] HandledBlocks
+		{
+			get
+			{
+				return new int[] { FreezeBombBlock.Index };
+			}
+		}
 
-		private SubsystemGameInfo m_subsystemGameInfo;
-		private SubsystemTime m_subsystemTime;
-		private SubsystemProjectiles m_subsystemProjectiles;
-		private SubsystemFreezeExplosions m_subsystemFreezeExplosions;
-		private SubsystemParticles m_subsystemParticles;
-		private SubsystemTerrain m_subsystemTerrain;
-		private SubsystemAudio m_subsystemAudio;
-		private SubsystemBodies m_subsystemBodies;
-		private Dictionary<Projectile, bool> m_projectiles = new Dictionary<Projectile, bool>();
+		public UpdateOrder UpdateOrder
+		{
+			get
+			{
+				return UpdateOrder.Default;
+			}
+		}
 
 		public override void Load(ValuesDictionary valuesDictionary)
 		{
 			base.Load(valuesDictionary);
-			m_subsystemGameInfo = Project.FindSubsystem<SubsystemGameInfo>(true);
-			m_subsystemTime = Project.FindSubsystem<SubsystemTime>(true);
-			m_subsystemProjectiles = Project.FindSubsystem<SubsystemProjectiles>(true);
-			m_subsystemFreezeExplosions = Project.FindSubsystem<SubsystemFreezeExplosions>(false);
-			m_subsystemParticles = Project.FindSubsystem<SubsystemParticles>(true);
-			m_subsystemTerrain = Project.FindSubsystem<SubsystemTerrain>(true);
-			m_subsystemAudio = Project.FindSubsystem<SubsystemAudio>(true);
-			m_subsystemBodies = Project.FindSubsystem<SubsystemBodies>(true);
-
-			foreach (Projectile projectile in m_subsystemProjectiles.Projectiles)
+			this.m_subsystemGameInfo = base.Project.FindSubsystem<SubsystemGameInfo>(true);
+			this.m_subsystemTime = base.Project.FindSubsystem<SubsystemTime>(true);
+			this.m_subsystemProjectiles = base.Project.FindSubsystem<SubsystemProjectiles>(true);
+			this.m_subsystemFreezeExplosions = base.Project.FindSubsystem<SubsystemFreezeExplosions>(false);
+			this.m_subsystemParticles = base.Project.FindSubsystem<SubsystemParticles>(true);
+			this.m_subsystemTerrain = base.Project.FindSubsystem<SubsystemTerrain>(true);
+			this.m_subsystemBodies = base.Project.FindSubsystem<SubsystemBodies>(true);
+			foreach (Projectile projectile in this.m_subsystemProjectiles.Projectiles)
 			{
-				ScanProjectile(projectile);
+				this.ScanProjectile(projectile);
 			}
-
-			m_subsystemProjectiles.ProjectileAdded += (Projectile projectile) => ScanProjectile(projectile);
-			m_subsystemProjectiles.ProjectileRemoved += (Projectile projectile) => m_projectiles.Remove(projectile);
+			this.m_subsystemProjectiles.ProjectileAdded += delegate (Projectile projectile)
+			{
+				this.ScanProjectile(projectile);
+			};
+			this.m_subsystemProjectiles.ProjectileRemoved += delegate (Projectile projectile)
+			{
+				this.m_projectiles.Remove(projectile);
+			};
 		}
 
-		private void ScanProjectile(Projectile projectile)
+		public void ScanProjectile(Projectile projectile)
 		{
-			if (!m_projectiles.ContainsKey(projectile))
+			if (!this.m_projectiles.ContainsKey(projectile))
 			{
 				int blockId = Terrain.ExtractContents(projectile.Value);
 				if (blockId == FreezeBombBlock.Index)
 				{
-					m_projectiles.Add(projectile, true);
+					this.m_projectiles.Add(projectile, true);
 					projectile.ProjectileStoppedAction = ProjectileStoppedAction.DoNothing;
 					Color blueColor = new Color(200, 230, 255);
-					m_subsystemProjectiles.AddTrail(projectile, new Vector3(0f, 0.25f, 0.1f), new SmokeTrailParticleSystem(15, 0.25f, float.MaxValue, blueColor));
+					this.m_subsystemProjectiles.AddTrail(projectile, new Vector3(0f, 0.25f, 0.1f), new SmokeTrailParticleSystem(15, 0.25f, float.MaxValue, blueColor));
 				}
 			}
 		}
 
 		public void Update(float dt)
 		{
-			if (m_subsystemTime.PeriodicGameTimeEvent(0.1, 0.0))
+			if (this.m_subsystemTime.PeriodicGameTimeEvent(0.1, 0.0))
 			{
-				List<Projectile> toRemove = new List<Projectile>();
-				foreach (Projectile projectile in m_projectiles.Keys)
+				int i = 0;
+				while (i < this.m_projectiles.Count)
 				{
-					if (m_subsystemGameInfo.TotalElapsedGameTime - projectile.CreationTime > 5.0)
+					Projectile projectile = this.m_projectiles.Keys.ElementAt(i);
+					if (this.m_subsystemGameInfo.TotalElapsedGameTime - projectile.CreationTime > 5.0)
 					{
-						CreateFreezeExplosion(projectile);
-						toRemove.Add(projectile);
+						this.CreateFreezeExplosion(projectile);
 						projectile.ToRemove = true;
+						this.m_projectiles.Remove(projectile);
+					}
+					else
+					{
+						i++;
 					}
 				}
-				foreach (Projectile p in toRemove)
-					m_projectiles.Remove(p);
 			}
 		}
 
-		private void CreateFreezeExplosion(Projectile projectile)
+		public void CreateFreezeExplosion(Projectile projectile)
 		{
-			Vector3 pos = projectile.Position;
-			int x = Terrain.ToCell(pos.X);
-			int y = Terrain.ToCell(pos.Y);
-			int z = Terrain.ToCell(pos.Z);
-
-			if (m_subsystemFreezeExplosions != null)
+			Vector3 position = projectile.Position;
+			int x = Terrain.ToCell(position.X);
+			int y = Terrain.ToCell(position.Y);
+			int z = Terrain.ToCell(position.Z);
+			if (this.m_subsystemFreezeExplosions != null)
 			{
-				m_subsystemFreezeExplosions.AddFreezeExplosion(x, y, z, 25f, 300f, false);
-			}
-			else
-			{
-				m_subsystemAudio?.PlaySound("Audio/explosion congelante", 1f, 0f, pos, 12f, true);
+				this.m_subsystemFreezeExplosions.AddFreezeExplosion(x, y, z, 25f, 300f, false);
 			}
 		}
 
 		public void TriggerFreezeExplosion(int x, int y, int z, int value)
 		{
-			m_subsystemTerrain.DestroyCell(0, x, y, z, value, false, false);
-			if (m_subsystemFreezeExplosions != null)
+			this.m_subsystemTerrain.DestroyCell(0, x, y, z, value, false, false);
+			if (this.m_subsystemFreezeExplosions != null)
 			{
-				m_subsystemFreezeExplosions.AddFreezeExplosion(x, y, z, 20f, 250f, true);
-			}
-			else
-			{
-				Vector3 pos = new Vector3(x + 0.5f, y + 0.5f, z + 0.5f);
-				m_subsystemAudio?.PlaySound("Audio/explosion congelante", 1f, 0f, pos, 10f, true);
+				this.m_subsystemFreezeExplosions.AddFreezeExplosion(x, y, z, 20f, 250f, true);
 			}
 		}
 
 		public override void OnHitByProjectile(CellFace cellFace, WorldItem worldItem)
 		{
-			int value = m_subsystemTerrain.Terrain.GetCellValue(cellFace.X, cellFace.Y, cellFace.Z);
+			int value = this.m_subsystemTerrain.Terrain.GetCellValue(cellFace.X, cellFace.Y, cellFace.Z);
 			int blockId = Terrain.ExtractContents(value);
 			if (blockId == FreezeBombBlock.Index)
 			{
-				TriggerFreezeExplosion(cellFace.X, cellFace.Y, cellFace.Z, value);
+				this.TriggerFreezeExplosion(cellFace.X, cellFace.Y, cellFace.Z, value);
 			}
 		}
 
@@ -129,12 +128,28 @@ namespace Game
 
 		public void HandleExplosionDamage(int x, int y, int z)
 		{
-			int cellValue = m_subsystemTerrain.Terrain.GetCellValue(x, y, z);
+			int cellValue = this.m_subsystemTerrain.Terrain.GetCellValue(x, y, z);
 			int blockId = Terrain.ExtractContents(cellValue);
 			if (blockId == FreezeBombBlock.Index)
 			{
-				TriggerFreezeExplosion(x, y, z, cellValue);
+				this.TriggerFreezeExplosion(x, y, z, cellValue);
 			}
 		}
+
+		public SubsystemGameInfo m_subsystemGameInfo;
+
+		public SubsystemTime m_subsystemTime;
+
+		public SubsystemProjectiles m_subsystemProjectiles;
+
+		public SubsystemFreezeExplosions m_subsystemFreezeExplosions;
+
+		public SubsystemParticles m_subsystemParticles;
+
+		public SubsystemTerrain m_subsystemTerrain;
+
+		public SubsystemBodies m_subsystemBodies;
+
+		public Dictionary<Projectile, bool> m_projectiles = new Dictionary<Projectile, bool>();
 	}
 }
