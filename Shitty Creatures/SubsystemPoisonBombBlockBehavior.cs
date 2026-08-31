df9ffa9898a4fab6dece1d77b90667a3ffe388dd
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-08-31 11:03:06 -0500

    Refactor variable access to use 'this' keyword

--- Shitty Creatures/SubsystemPoisonBombBlockBehavior.cs
+++ Shitty Creatures/SubsystemPoisonBombBlockBehavior.cs
@@ -27,178 +27,128 @@ namespace Game
 		public override void Load(ValuesDictionary valuesDictionary)
 		{
 			base.Load(valuesDictionary);
-
-			m_subsystemGameInfo = base.Project.FindSubsystem<SubsystemGameInfo>(true);
-			m_subsystemTime = base.Project.FindSubsystem<SubsystemTime>(true);
-			m_subsystemProjectiles = base.Project.FindSubsystem<SubsystemProjectiles>(true);
-			m_subsystemPoisonExplosions = base.Project.FindSubsystem<SubsystemPoisonExplosions>(false);
-			m_subsystemParticles = base.Project.FindSubsystem<SubsystemParticles>(true);
-			m_subsystemTerrain = base.Project.FindSubsystem<SubsystemTerrain>(true);
-			m_subsystemAudio = base.Project.FindSubsystem<SubsystemAudio>(true);
-			m_subsystemBodies = base.Project.FindSubsystem<SubsystemBodies>(true);
-
-			foreach (Projectile projectile in m_subsystemProjectiles.Projectiles)
+			this.m_subsystemGameInfo = base.Project.FindSubsystem<SubsystemGameInfo>(true);
+			this.m_subsystemTime = base.Project.FindSubsystem<SubsystemTime>(true);
+			this.m_subsystemProjectiles = base.Project.FindSubsystem<SubsystemProjectiles>(true);
+			this.m_subsystemPoisonExplosions = base.Project.FindSubsystem<SubsystemPoisonExplosions>(false);
+			this.m_subsystemParticles = base.Project.FindSubsystem<SubsystemParticles>(true);
+			this.m_subsystemTerrain = base.Project.FindSubsystem<SubsystemTerrain>(true);
+			this.m_subsystemBodies = base.Project.FindSubsystem<SubsystemBodies>(true);
+			foreach (Projectile projectile in this.m_subsystemProjectiles.Projectiles)
 			{
-				ScanProjectile(projectile);
+				this.ScanProjectile(projectile);
 			}
-
-			SubsystemProjectiles subsystemProjectiles = m_subsystemProjectiles;
-			subsystemProjectiles.ProjectileAdded += delegate (Projectile projectile)
+			this.m_subsystemProjectiles.ProjectileAdded += delegate (Projectile projectile)
 			{
-				ScanProjectile(projectile);
+				this.ScanProjectile(projectile);
 			};
-
-			SubsystemProjectiles subsystemProjectiles2 = m_subsystemProjectiles;
-			subsystemProjectiles2.ProjectileRemoved += delegate (Projectile projectile)
+			this.m_subsystemProjectiles.ProjectileRemoved += delegate (Projectile projectile)
 			{
-				m_projectiles.Remove(projectile);
+				this.m_projectiles.Remove(projectile);
 			};
 		}
 
 		public void ScanProjectile(Projectile projectile)
 		{
-			if (!m_projectiles.ContainsKey(projectile))
+			if (!this.m_projectiles.ContainsKey(projectile))
 			{
 				int blockId = Terrain.ExtractContents(projectile.Value);
-
-				// Solo manejar bombas de veneno
 				if (blockId == PoisonBombBlock.Index)
 				{
-					m_projectiles.Add(projectile, true);
+					this.m_projectiles.Add(projectile, true);
 					projectile.ProjectileStoppedAction = ProjectileStoppedAction.DoNothing;
-
-					// Añadir partículas de rastro verdes
 					Color greenColor = new Color(51, 255, 51);
-					m_subsystemProjectiles.AddTrail(projectile,
-						new Vector3(0f, 0.25f, 0.1f),
-						new SmokeTrailParticleSystem(15, 0.25f, float.MaxValue, greenColor));
+					this.m_subsystemProjectiles.AddTrail(projectile, new Vector3(0f, 0.25f, 0.1f), new SmokeTrailParticleSystem(15, 0.25f, float.MaxValue, greenColor));
 				}
 			}
 		}
 
 		public void Update(float dt)
 		{
-			if (m_subsystemTime.PeriodicGameTimeEvent(0.1, 0.0))
+			if (this.m_subsystemTime.PeriodicGameTimeEvent(0.1, 0.0))
 			{
-				List<Projectile> projectilesToRemove = new List<Projectile>();
-
-				foreach (Projectile projectile in m_projectiles.Keys)
+				int i = 0;
+				while (i < this.m_projectiles.Count)
 				{
-					// Explotar después de 5 segundos
-					if (m_subsystemGameInfo.TotalElapsedGameTime - projectile.CreationTime > 5.0)
+					Projectile projectile = this.m_projectiles.Keys.ElementAt(i);
+					if (this.m_subsystemGameInfo.TotalElapsedGameTime - projectile.CreationTime > 5.0)
 					{
-						CreatePoisonExplosion(projectile);
-						projectilesToRemove.Add(projectile);
+						this.CreatePoisonExplosion(projectile);
 						projectile.ToRemove = true;
+						this.m_projectiles.Remove(projectile);
 					}
-				}
-
-				// Eliminar proyectiles procesados
-				foreach (Projectile projectile in projectilesToRemove)
-				{
-					m_projectiles.Remove(projectile);
+					else
+					{
+						i++;
+					}
 				}
 			}
 		}
 
-		private void CreatePoisonExplosion(Projectile projectile)
+		public void CreatePoisonExplosion(Projectile projectile)
 		{
 			Vector3 position = projectile.Position;
 			int x = Terrain.ToCell(position.X);
 			int y = Terrain.ToCell(position.Y);
 			int z = Terrain.ToCell(position.Z);
-
-			// Usar el sistema de explosiones de veneno si existe
-			if (m_subsystemPoisonExplosions != null)
+			if (this.m_subsystemPoisonExplosions != null)
 			{
-				float explosionPressure = 25f;
-				float poisonIntensity = 180f;
-
-				m_subsystemPoisonExplosions.AddPoisonExplosion(
-					x, y, z,
-					explosionPressure,
-					poisonIntensity,
-					false
-				);
-			}
-			else
-			{
-				// Si no existe el sistema de explosiones de veneno, reproducir sonido
-				if (m_subsystemAudio != null)
-				{
-					m_subsystemAudio.PlaySound("Audio/Explosion De Mierda/Smoke Explosion", 1f, 0f, position, 12f, true);
-				}
+				this.m_subsystemPoisonExplosions.AddPoisonExplosion(x, y, z, 25f, 180f, false);
 			}
 		}
 
-		// Método para manejar cuando una bomba de veneno es golpeada por un proyectil
 		public void TriggerPoisonExplosion(int x, int y, int z, int value)
 		{
-			// Destruir el bloque
-			m_subsystemTerrain.DestroyCell(0, x, y, z, value, false, false);
-
-			// Crear explosión de veneno usando el sistema si existe
-			if (m_subsystemPoisonExplosions != null)
+			this.m_subsystemTerrain.DestroyCell(0, x, y, z, value, false, false);
+			if (this.m_subsystemPoisonExplosions != null)
 			{
-				m_subsystemPoisonExplosions.AddPoisonExplosion(
-					x, y, z,
-					20f,   // Presión
-					150f,  // Intensidad
-					true
-				);
-			}
-			else if (m_subsystemAudio != null)
-			{
-				Vector3 position = new Vector3(x + 0.5f, y + 0.5f, z + 0.5f);
-				m_subsystemAudio.PlaySound("Audio/Explosion De Mierda/Smoke Explosion", 1f, 0f, position, 10f, true);
+				this.m_subsystemPoisonExplosions.AddPoisonExplosion(x, y, z, 20f, 150f, true);
 			}
 		}
 
 		public override void OnHitByProjectile(CellFace cellFace, WorldItem worldItem)
 		{
-			// Cuando la bomba es golpeada por un proyectil
-			int value = m_subsystemTerrain.Terrain.GetCellValue(cellFace.X, cellFace.Y, cellFace.Z);
+			int value = this.m_subsystemTerrain.Terrain.GetCellValue(cellFace.X, cellFace.Y, cellFace.Z);
 			int blockId = Terrain.ExtractContents(value);
-
-			// Verificar que sea una bomba de veneno
 			if (blockId == PoisonBombBlock.Index)
 			{
-				TriggerPoisonExplosion(cellFace.X, cellFace.Y, cellFace.Z, value);
+				this.TriggerPoisonExplosion(cellFace.X, cellFace.Y, cellFace.Z, value);
 			}
 		}
 
 		public override bool OnInteract(TerrainRaycastResult raycastResult, ComponentMiner componentMiner)
 		{
-			// Permitir recoger la bomba sin que explote
 			return false;
 		}
 
 		public override void OnItemHarvested(int x, int y, int z, int blockValue, ref BlockDropValue dropValue, ref int newBlockValue)
 		{
-			// Cuando se mina la bomba, asegurarse de que no explote
 		}
 
-		// Método para manejar cuando la bomba es destruida por una explosión
 		public void HandleExplosionDamage(int x, int y, int z)
 		{
-			// Verificar si hay una bomba de veneno en esta posición
-			int cellValue = m_subsystemTerrain.Terrain.GetCellValue(x, y, z);
+			int cellValue = this.m_subsystemTerrain.Terrain.GetCellValue(x, y, z);
 			int blockId = Terrain.ExtractContents(cellValue);
-
 			if (blockId == PoisonBombBlock.Index)
 			{
-				TriggerPoisonExplosion(x, y, z, cellValue);
+				this.TriggerPoisonExplosion(x, y, z, cellValue);
 			}
 		}
 
-		private SubsystemGameInfo m_subsystemGameInfo;
-		private SubsystemTime m_subsystemTime;
-		private SubsystemProjectiles m_subsystemProjectiles;
-		private SubsystemPoisonExplosions m_subsystemPoisonExplosions;
-		private SubsystemParticles m_subsystemParticles;
-		private SubsystemTerrain m_subsystemTerrain;
-		private SubsystemAudio m_subsystemAudio;
-		private SubsystemBodies m_subsystemBodies;
-		private Dictionary<Projectile, bool> m_projectiles = new Dictionary<Projectile, bool>();
+		public SubsystemGameInfo m_subsystemGameInfo;
+
+		public SubsystemTime m_subsystemTime;
+
+		public SubsystemProjectiles m_subsystemProjectiles;
+
+		public SubsystemPoisonExplosions m_subsystemPoisonExplosions;
+
+		public SubsystemParticles m_subsystemParticles;
+
+		public SubsystemTerrain m_subsystemTerrain;
+
+		public SubsystemBodies m_subsystemBodies;
+
+		public Dictionary<Projectile, bool> m_projectiles = new Dictionary<Projectile, bool>();
 	}
 }
