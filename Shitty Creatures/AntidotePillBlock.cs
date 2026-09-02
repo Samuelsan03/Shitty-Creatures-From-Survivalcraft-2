330a08f2e8d443a4693e6c351bfe15f40b6baa82
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-09-01 22:04:08 -0500

    Add AntidotePillBlock class for antidote pill block

---
+++ Shitty Creatures/AntidotePillBlock.cs
@@ -0,0 +1,14 @@
+using System;
+using Engine;
+using Engine.Graphics;
+
+namespace Game
+{
+	public class AntidotePillBlock : ShittyTexturesFlat
+	{
+		public AntidotePillBlock() : base("Textures/pastilla")
+		{
+			this.BlockIndex = 536;
+		}
+	}
+}
