80e873b0b16a6a41278a462e3edbeffcbca41536
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-09-16 15:46:23 -0500

    Create BestiaryCreatureCategory.cs

---
+++ Shitty Creatures/BestiaryCreatureCategory.cs
@@ -0,0 +1,13 @@
+namespace Game
+{
+	/// <summary>
+	/// Categorías detectadas por el bestiario.
+	/// Infectado = criaturas infectadas.
+	/// Bandido   = bandidos añadidos por el huevo de bandidos.
+	/// </summary>
+	public enum BestiaryCreatureCategory
+	{
+		Infected,
+		Bandit
+	}
+}
